89455775b6ae63ffd6d93103036e25297dcfc527
github-action-benchmark <github@users.noreply.github.com>
2021-12-04 15:02:13 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for d7de88a18273e209c95354be1072407d9bee4609

---
+++ dev/bench/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1638630133723,
+  "repoUrl": "https://github.com/gametaro/dotfiles",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "32237320+gametaro@users.noreply.github.com",
+            "name": "Kotaro Yamada",
+            "username": "gametaro"
+          },
+          "committer": {
+            "email": "gametaro@outlook.com",
+            "name": "Kotaro Yamada",
+            "username": "gametaro"
+          },
+          "distinct": true,
+          "id": "d7de88a18273e209c95354be1072407d9bee4609",
+          "message": "Update benchmark.sh",
+          "timestamp": "2021-12-04T23:51:54+09:00",
+          "tree_id": "d22ea7ff8d22e9a00698ad89cfbf5223b6cfa8f1",
+          "url": "https://github.com/gametaro/dotfiles/commit/d7de88a18273e209c95354be1072407d9bee4609"
+        },
+        "date": 1638630132901,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Neovim startup time",
+            "value": 0.595,
+            "unit": "Second"
+          }
+        ]
+      }
+    ]
+  }
+}
