53dc3115b8f90516a74bee3f4f6c34dea14d7e6e
github-action-benchmark <github@users.noreply.github.com>
2021-12-04 18:39:04 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for d8bc5f7ad03da2db78e421ed5d8d048a32afa3cb

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1638630133723,
+  "lastUpdate": 1638643144063,
   "repoUrl": "https://github.com/gametaro/dotfiles",
   "entries": {
     "Benchmark": [
@@ -28,6 +28,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "Neovim startup time",
             "value": 0.595,
+            "unit": "Second"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "32237320+gametaro@users.noreply.github.com",
+            "name": "Kotaro Yamada",
+            "username": "gametaro"
+          },
+          "committer": {
+            "email": "gametaro@outlook.com",
+            "name": "Kotaro Yamada",
+            "username": "gametaro"
+          },
+          "distinct": true,
+          "id": "d8bc5f7ad03da2db78e421ed5d8d048a32afa3cb",
+          "message": "Update benchmark.sh",
+          "timestamp": "2021-12-05T03:37:37+09:00",
+          "tree_id": "4663681e8fc1d44a79e6e1f689dd96ba5b8f0609",
+          "url": "https://github.com/gametaro/dotfiles/commit/d8bc5f7ad03da2db78e421ed5d8d048a32afa3cb"
+        },
+        "date": 1638643143235,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Neovim Startup time - Mean",
+            "value": 0.7035313928400001,
+            "unit": "Second"
+          },
+          {
+            "name": "Neovim Startup time - Median",
+            "value": 0.6992980884400001,
+            "unit": "Second"
+          },
+          {
+            "name": "Neovim Startup time - Min",
+            "value": 0.66696835594,
+            "unit": "Second"
+          },
+          {
+            "name": "Neovim Startup time - Max",
+            "value": 0.7311980999400001,
             "unit": "Second"
           }
         ]
