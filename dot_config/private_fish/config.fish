2220d779a75718c079aa6a55f75cccdca51127c7
Kotaro Yamada <gametaro@outlook.com>
2021-12-05 11:44:11 +0900

    chore(shell): add ~/.local/bin to PATH

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -6,6 +6,10 @@ set -Ux VISUAL nvim
 abbr v nvim
 abbr g git
 
+if test -d $HOME/.local/bin
+  set PATH $HOME/.local/bin $PATH
+end
+
 source ~/.asdf/asdf.fish
 if ! test -f $HOME/.config/fish/completions/asdf.fish
   mkdir -p $HOME/.config/fish/completions; and ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
