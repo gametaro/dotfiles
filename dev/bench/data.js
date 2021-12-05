54e0e838a4ff6faf58bbdbd199c3ac58f91d520e
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2021-12-05 13:53:22 +0900

    Update data.js

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -16,35 +16,6 @@ window.BENCHMARK_DATA = {
             "username": "gametaro"
           },
           "distinct": true,
-          "id": "d7de88a18273e209c95354be1072407d9bee4609",
-          "message": "Update benchmark.sh",
-          "timestamp": "2021-12-04T23:51:54+09:00",
-          "tree_id": "d22ea7ff8d22e9a00698ad89cfbf5223b6cfa8f1",
-          "url": "https://github.com/gametaro/dotfiles/commit/d7de88a18273e209c95354be1072407d9bee4609"
-        },
-        "date": 1638630132901,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Neovim startup time",
-            "value": 0.595,
-            "unit": "Second"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "32237320+gametaro@users.noreply.github.com",
-            "name": "Kotaro Yamada",
-            "username": "gametaro"
-          },
-          "committer": {
-            "email": "gametaro@outlook.com",
-            "name": "Kotaro Yamada",
-            "username": "gametaro"
-          },
-          "distinct": true,
           "id": "d8bc5f7ad03da2db78e421ed5d8d048a32afa3cb",
           "message": "Update benchmark.sh",
           "timestamp": "2021-12-05T03:37:37+09:00",
