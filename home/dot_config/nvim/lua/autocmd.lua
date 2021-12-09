c5decdda0095d9cfef5227e5ab403d5c84b4d2df
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2021-12-09 20:38:00 +0900

    chore(autocmd): replace all cmd with vim.cmd for treesitter syntax

--- home/dot_config/nvim/lua/autocmd.lua
+++ home/dot_config/nvim/lua/autocmd.lua
@@ -1,32 +1,30 @@
-local cmd = vim.cmd
-
-cmd [[
+vim.cmd [[
 augroup mine
 autocmd!
 augroup END
 ]]
 
 -- automatically open the quickfix window
-cmd [[
+vim.cmd [[
 autocmd mine QuickFixCmdPost [^l]* nested cwindow
 autocmd mine QuickFixCmdPost l* nested lwindow
 ]]
 
 -- close with "q"
 local close_filetypes = { 'help', 'capture', 'lspinfo', 'vim', 'qf' }
-cmd(string.format('autocmd mine FileType %s nnoremap <buffer> q <C-w>c', table.concat(close_filetypes, ',')))
+vim.cmd(string.format('autocmd mine FileType %s nnoremap <buffer> q <C-w>c', table.concat(close_filetypes, ',')))
 
 -- highlight on yank
-cmd 'autocmd mine TextYankPost * lua vim.highlight.on_yank {higroup = "Search", timeout = 250}'
+vim.cmd 'autocmd mine TextYankPost * lua vim.highlight.on_yank {higroup = "Search", timeout = 250}'
 
 -- configure VIM to run chezmoi apply whenever a dotfile is saved
-cmd 'autocmd mine BufWritePost ~/.local/share/chezmoi/* silent !chezmoi apply --source-path %'
+vim.cmd 'autocmd mine BufWritePost ~/.local/share/chezmoi/* silent !chezmoi apply --source-path %'
 
 -- tweak some colors
-cmd 'autocmd mine ColorScheme * highlight DiffDelete guifg=#725272 gui = bold'
+vim.cmd 'autocmd mine ColorScheme * highlight DiffDelete guifg=#725272 gui = bold'
 
 -- show cursor line only in active window
-cmd [[
+vim.cmd [[
 autocmd mine InsertLeave,WinEnter * set cursorline
 autocmd mine InsertEnter,WinLeave * set nocursorline
 ]]
@@ -35,15 +33,15 @@ autocmd mine InsertEnter,WinLeave * set nocursorline
 -- cmd 'autocmd mine BufWritePost */lua/*.lua source <afile> | PackerCompile'
 
 -- always jump to the last cursor position
-cmd [[
+vim.cmd [[
 autocmd mine BufReadPost * if line("'\"") > 0 && line("'\"") <= line("$") | execute "normal! g`\"" | endif
 ]]
 
-cmd 'autocmd mine FocusLost * wall'
-cmd 'autocmd mine BufLeave * lua if require"utils".can_save() then vim.cmd "silent! update" end'
+vim.cmd 'autocmd mine FocusLost * wall'
+vim.cmd 'autocmd mine BufLeave * lua if require"utils".can_save() then vim.cmd "silent! update" end'
 
-cmd 'autocmd mine BufWritePost * if &diff | diffupdate | endif'
+vim.cmd 'autocmd mine BufWritePost * if &diff | diffupdate | endif'
 
-cmd 'autocmd mine FileType qf set nobuflisted'
+vim.cmd 'autocmd mine FileType qf set nobuflisted'
 
--- cmd 'autocmd mine CursorHold,CursorHoldI * lua vim.lsp.diagnostic.show_line_diagnostics { focusable = false }'
+-- vim.cmd 'autocmd mine CursorHold,CursorHoldI * lua vim.lsp.diagnostic.show_line_diagnostics { focusable = false }'
