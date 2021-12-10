d6258e3add41d4d3e73130019b751f13cb175484
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2021-12-11 03:31:59 +0900

    feat(lir): add action to create file or directory smarter

--- home/dot_config/nvim/lua/config/lir.lua
+++ home/dot_config/nvim/lua/config/lir.lua
@@ -1,6 +1,86 @@
+local lir = require 'lir'
+local utils = require 'lir.utils'
+local config = require 'lir.config'
 local actions = require 'lir.actions'
 local mark_actions = require 'lir.mark.actions'
 local clipboard_actions = require 'lir.clipboard.actions'
+local Path = require 'plenary.path'
+
+local function lcd(path)
+  vim.cmd(string.format('lcd %s', path))
+end
+
+local no_confirm_patterns = {
+  '^LICENSE$',
+  '^Makefile$',
+}
+
+local function need_confirm(filename)
+  for _, pattern in ipairs(no_confirm_patterns) do
+    if filename:match(pattern) then
+      return false
+    end
+  end
+  return true
+end
+
+-- NOTE: https://github.com/tamago324/lir.nvim/wiki/Custom-actions#input_newfile
+local function create()
+  local save_curdir = vim.fn.getcwd()
+  lcd(lir.get_context().dir)
+  local name = vim.fn.input('New file: ', '', 'file')
+  lcd(save_curdir)
+
+  if name == '' then
+    return
+  end
+
+  if name == '.' or name == '..' then
+    utils.error('Invalid file name: ' .. name)
+    return
+  end
+
+  -- If I need to check, I will.
+  if need_confirm(name) then
+    -- '.' is not included or '/' is not included, then
+    -- I may have entered it as a directory, I'll check.
+    if not name:match '%.' and not name:match '/' then
+      if vim.fn.confirm('Directory?', '&No\n&Yes', 1) == 2 then
+        name = name .. '/'
+      end
+    end
+  end
+
+  local path = Path:new(lir.get_context().dir .. name)
+  if string.match(name, '/$') then
+    -- mkdir
+    name = name:gsub('/$', '')
+    path:mkdir {
+      parents = true,
+      mode = tonumber('700', 8),
+      exists_ok = false,
+    }
+  else
+    -- touch
+    path:touch {
+      parents = true,
+      mode = tonumber('644', 8),
+    }
+  end
+
+  -- If the first character is '.' and show_hidden_files is false, set it to true
+  if name:match [[^%.]] and not config.values.show_hidden_files then
+    config.values.show_hidden_files = true
+  end
+
+  actions.reload()
+
+  -- Jump to a line in the parent directory of the file you created.
+  local lnum = lir.get_context():indexof(name:match '^[^/]+')
+  if lnum then
+    vim.cmd(tostring(lnum))
+  end
+end
 
 require('lir').setup {
   hide_cursor = false,
@@ -18,7 +98,8 @@ require('lir').setup {
     ['q'] = actions.quit,
 
     ['m'] = actions.mkdir,
-    ['a'] = actions.newfile,
+    -- ['a'] = actions.newfile,
+    ['a'] = create,
     ['r'] = actions.rename,
     ['@'] = actions.cd,
     ['y'] = actions.yank_path,
