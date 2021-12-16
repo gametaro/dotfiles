02dd3a3fddb4fdb632dfa8640ba89d2496caf98e
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2021-12-16 19:33:49 +0900

    refactor(options): move global variables to init.lua

--- home/dot_config/nvim/plugin/options.lua
+++ home/dot_config/nvim/plugin/options.lua
@@ -1,34 +1,6 @@
 local g, opt = vim.g, vim.opt
 local indent = 2
 
-g.mapleader = ' '
-g.maplocalleader = ','
-
-g.did_load_filetypes = 1
-g.loaded_2html_plugin = 1
-g.loaded_getscript = 1
-g.loaded_getscriptPlugin = 1
-g.loaded_gzip = 1
-g.loaded_logiPat = 1
-g.loaded_man = 1
-g.loaded_matchit = 1
-g.loaded_matchparen = 1
-g.loaded_netrw = 1
-g.loaded_netrwFileHandlers = 1
-g.loaded_netrwPlugin = 1
-g.loaded_netrwSettings = 1
-g.loaded_rrhelper = 1
-g.loaded_shada_plugin = 1
-g.loaded_tar = 1
-g.loaded_tarPlugin = 1
-g.loaded_tutor_mode_plugin = 1
-g.loaded_vimball = 1
-g.loaded_vimballPlugin = 1
-g.loaded_zip = 1
-g.loaded_zipPlugin = 1
-g.netrw_nogx = 1
-g.qf_disable_statusline = 1
-
 if vim.fn.exists 'g:neovide' > 0 then
   g.neovide_cursor_animation_length = 0
   g.neovide_cursor_trail_length = 0
@@ -38,7 +10,6 @@ end
 opt.autowriteall = true
 opt.clipboard = 'unnamedplus'
 opt.completeopt = { 'menu', 'menuone', 'noselect', 'noinsert' }
-opt.cursorline = true
 opt.diffopt = {
   'algorithm:histogram',
   'closeoff',
