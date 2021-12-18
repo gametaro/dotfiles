b0413fb26d8f469b19db04625a46c726dd230d6d
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2021-12-18 20:54:28 +0900

    fix(init): don't defer sourcing plugins.lua

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -3,7 +3,5 @@ vim.g.maplocalleader = ','
 
 require 'ky.disable'
 pcall(require, 'impatient')
-vim.defer_fn(function()
-  require 'ky.plugins'
-end, 0)
+require 'ky.plugins'
 require('ky.theme').setup 'nightfox'
