7ea3ed904c6f5558acc6c4b049b0c7151ba7ddc5
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2021-12-18 19:58:56 +0900

    refactor(init): move `disable builtins` to separate file

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -1,33 +1,7 @@
-local g = vim.g
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ','
 
-g.mapleader = ' '
-g.maplocalleader = ','
-
-g.did_load_filetypes = 1
-g.loaded_2html_plugin = 1
-g.loaded_getscript = 1
-g.loaded_getscriptPlugin = 1
-g.loaded_gzip = 1
-g.loaded_logiPat = 1
-g.loaded_man = 1
-g.loaded_matchit = 1
-g.loaded_matchparen = 1
-g.loaded_netrw = 1
-g.loaded_netrwFileHandlers = 1
-g.loaded_netrwPlugin = 1
-g.loaded_netrwSettings = 1
-g.loaded_rrhelper = 1
-g.loaded_shada_plugin = 1
-g.loaded_tar = 1
-g.loaded_tarPlugin = 1
-g.loaded_tutor_mode_plugin = 1
-g.loaded_vimball = 1
-g.loaded_vimballPlugin = 1
-g.loaded_zip = 1
-g.loaded_zipPlugin = 1
-g.netrw_nogx = 1
-g.qf_disable_statusline = 1
-
+require 'ky.disable'
 pcall(require, 'impatient')
 vim.defer_fn(function()
   require('ky.plugins')
