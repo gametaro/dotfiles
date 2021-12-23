13658f9d1865d22a83f8e89513b396a4624c4fe9
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2021-12-24 02:27:22 +0900

    refactor(null-ls): simplify code

--- home/dot_config/nvim/lua/ky/lsp/null-ls.lua
+++ home/dot_config/nvim/lua/ky/lsp/null-ls.lua
@@ -12,40 +12,40 @@ local function condition(cmd)
 end
 
 local sources = {
-  b.formatting.prettierd.with {
+  f.prettierd.with {
     condition = condition 'prettierd',
     filetypes = { 'html', 'yaml', 'markdown', 'javascript', 'typescript' },
   },
-  b.formatting.shellharden.with {
+  f.shellharden.with {
     condition = condition 'shellharden',
   },
-  b.formatting.shfmt.with {
+  f.shfmt.with {
     condition = condition 'shfmt',
     extra_args = { '-i', '2', '-bn', '-ci', '-kp' },
   },
-  b.formatting.stylua.with {
+  f.stylua.with {
     condition = condition 'stylua',
     extra_args = { '--config-path', vim.fn.stdpath 'config' .. '/stylua.toml' },
   },
-  b.formatting.autopep8.with {
+  f.autopep8.with {
     condition = condition 'autopep8',
   },
-  b.formatting.fish_indent.with {
+  f.fish_indent.with {
     condition = condition 'fish_indent',
   },
-  b.diagnostics.markdownlint.with {
+  d.markdownlint.with {
     condition = condition 'markdownlint',
   },
-  b.diagnostics.shellcheck.with {
+  d.shellcheck.with {
     condition = condition 'shellcheck',
   },
-  b.diagnostics.flake8.with {
+  d.flake8.with {
     condition = condition 'flake8',
   },
-  b.diagnostics.pylint.with {
+  d.pylint.with {
     condition = condition 'pylint',
   },
-  b.diagnostics.codespell.with {
+  d.codespell.with {
     disabled_filetypes = { "NeogitCommitMessage" },
     condition = condition 'codespell',
   },
