afbeeef7f7a99d3caec578c95348b917028cb782
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2021-12-23 23:16:08 +0900

    feat(mappings): update mappings

--- home/dot_config/nvim/lua/ky/utils.lua
+++ home/dot_config/nvim/lua/ky/utils.lua
@@ -4,32 +4,69 @@ function _G.P(...)
   print(vim.inspect(...))
 end
 
+---check whether running neovim version is nightly or not
+---check if the version of the running neovim is nightly
+---@return boolean
 function M.is_nightly()
   return vim.fn.has 'nvim-0.7' > 0
 end
 
-function M.toggle_qf()
+--toggle quickfix window
+--@return any
+function M.toggle_quickfix()
   return vim.fn.getqflist({ winid = 0 }).winid ~= 0 and vim.cmd 'cclose' or vim.cmd 'botright copen'
 end
 
-function M.goto_qf()
+---go to quickfix window
+---@return any
+function M.goto_quickfix()
   local winid = vim.fn.getqflist({ winid = 0 }).winid
   vim.cmd 'cwindow'
   return winid ~= 0 and vim.fn.win_gotoid(winid)
 end
 
+---go to loclist window
+---@return any
 function M.goto_loclist()
   local winid = vim.fn.getloclist({ winid = 0 }).winid
   vim.cmd 'lwindow'
   return winid ~= 0 and vim.fn.win_gotoid(winid)
 end
 
+---check if the current buffer can be saved
+---@return boolean
 function M.can_save()
   return vim.bo.buftype == '' and vim.bo.filetype ~= '' and vim.bo.modifiable
 end
 
+---check nvim is running on headless mode
+---@return boolean
 function M.not_headless()
   return #vim.api.nvim_list_uis() > 0
+end
+
+---Searches process tree for a process having a name in the `names` list.
+---Limited breadth/depth.
+---@param rootpid number
+---@param names list
+---@param acc number
+---@return boolean
+-- @see https://github.com/justinmk/config/blob/master/.config/nvim/init.vim
+function M.find_proc_in_tree(rootpid, names, acc)
+  if acc > 9 or not vim.fn.exists '*nvim_get_proc' then
+    return false
+  end
+  local p = vim.api.nvim_get_proc(rootpid)
+  if vim.fn.empty(p) ~= 1 and vim.tbl_contains(names, p.name) then
+    return true
+  end
+  local ids = vim.api.nvim_get_proc_children(rootpid)
+  for _, id in ipairs(ids) do
+    if M.find_proc_in_tree(id, names, 1 + acc) then
+      return true
+    end
+  end
+  return false
 end
 
 return M
--- home/dot_config/nvim/plugin/mappings.lua
+++ home/dot_config/nvim/plugin/mappings.lua
@@ -2,9 +2,27 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 -- Nop
-map('n', '<C-z>', '<Nop>', opts)
-map('n', 'ZQ', '<Nop>', opts)
-map('n', 'ZZ', '<Nop>', opts)
+map('n', '+', '<Nop>', {})
+map('n', '<C-h>', '<Nop>', {})
+map('n', '<C-j>', '<Nop>', {})
+map('n', '<C-k>', '<Nop>', {})
+map('n', '<C-n>', '<Nop>', {})
+map('n', '<C-p>', '<Nop>', {})
+map('n', '<C-z>', '<Nop>', {})
+map('n', '<S-Tab>', '<Nop>', {})
+map('n', '<Tab>', '<Nop>', {})
+map('n', 'ZQ', '<Nop>', {})
+map('n', 'ZZ', '<Nop>', {})
+map('n', '~', '<Nop>', {})
+map('c', '<C-_>', '<Nop>', {})
+map('c', '<C-j>', '<Nop>', {})
+map('c', '<C-o>', '<Nop>', {})
+map('c', '<C-x>', '<Nop>', {})
+map('c', '<C-z>', '<Nop>', {})
+map('i', '<C-_>', '<Nop>', {})
+map('i', '<C-b>', '<Nop>', {})
+map('i', '<C-z>', '<Nop>', {})
+map('i', '<C-z>', '<Nop>', {})
 
 map('x', '=', '=gv', opts)
 
@@ -13,8 +31,17 @@ map('n', '<Leader>c', '"_c', opts)
 map('n', '<Leader>d', '"_d', opts)
 
 -- swap ; for :
-map('n', ';', ':', { noremap = true })
-map('n', ':', ';', { noremap = true })
+map('', ';', ':', { noremap = true })
+map('', ':', ';', { noremap = true })
+
+-- swap w, b, e for W, B, E (normal mode only)
+-- WARN: experimental
+map('n', 'w', 'W', { noremap = true })
+map('n', 'W', 'w', { noremap = true })
+map('n', 'b', 'B', { noremap = true })
+map('n', 'B', 'b', { noremap = true })
+map('n', 'e', 'E', { noremap = true })
+map('n', 'E', 'e', { noremap = true })
 
 map('n', '<Leader>w', '<Cmd>update<CR>', opts)
 map('n', '<Leader>W', '<Cmd>update!<CR>', opts)
@@ -29,35 +56,76 @@ map('c', '<C-f>', '<Right>', { noremap = true })
 map('c', '<C-n>', '<Down>', { noremap = true })
 map('c', '<C-p>', '<Up>', { noremap = true })
 
+-- map('n', '/', '/\v', { noremap = true })
+-- map('n', '?', '?\v', { noremap = true })
 map('c', '%%', [[getcmdtype() == ':' ? expand('%:h').'/' : '%%']], { noremap = true, expr = true })
 map('c', '::', [[getcmdtype() == ':' ? expand('%:p:h').'/' : '::']], { noremap = true, expr = true })
 map('c', '/', [[getcmdtype() == '/' ? '\/' : '/']], { noremap = true, expr = true })
 map('c', '?', [[getcmdtype() == '?' ? '\?' : '?']], { noremap = true, expr = true })
 
+-- use `ALT+{h,j,k,l}` to navigate windows from any mode
+-- @see :h terminal
 map('n', '<M-h>', '<C-w>h', opts)
 map('n', '<M-j>', '<C-w>j', opts)
 map('n', '<M-k>', '<C-w>k', opts)
 map('n', '<M-l>', '<C-w>l', opts)
-map('i', '<M-h>', '<Esc><C-w>h', opts)
-map('i', '<M-j>', '<Esc><C-w>j', opts)
-map('i', '<M-k>', '<Esc><C-w>k', opts)
-map('i', '<M-l>', '<Esc><C-w>l', opts)
+map('i', '<M-h>', [[<C-\><C-n><C-w>h]], opts)
+map('i', '<M-j>', [[<C-\><C-n><C-w>j]], opts)
+map('i', '<M-k>', [[<C-\><C-n><C-w>k]], opts)
+map('i', '<M-l>', [[<C-\><C-n><C-w>l]], opts)
+map('t', '<M-h>', [[<C-\><C-n><C-W>h]], opts)
+map('t', '<M-j>', [[<C-\><C-n><C-W>j]], opts)
+map('t', '<M-k>', [[<C-\><C-n><C-W>k]], opts)
+map('t', '<M-l>', [[<C-\><C-n><C-W>l]], opts)
+map('t', '<Esc>', [[<C-\><C-n>]], opts)
 
--- map('n', '<Leader>q', '<Cmd>lua require"ky.utils".toggle_qf()<CR>', opts)
+map('n', '<M-,>', '<Cmd>bprevious<CR>', opts)
+map('n', '<M-.>', '<Cmd>bnext<CR>', opts)
+map('n', '<M-c>', '<Cmd>bdelete<CR>', opts)
+map('n', '<M-C>', '<Cmd>bdelete!<CR>', opts)
 
--- NOTE: https://github.com/mhinz/vim-galore#quickly-add-empty-lines
+-- @see https://github.com/justinmk/config/blob/master/.config/nvim/init.vim
+map('c', '<M-e>', "<C-r>=fnameescape('')<Left><Left>", { noremap = true })
+map('c', '<M-f>', "<C-r>=fnamemodify(@%, ':t')<CR>", { noremap = true })
+map('c', '<M-/>', [[\v^(()@!.)*$<Left><Left><Left><Left><Left><Left><Left>]], { noremap = true })
+map('n', 'gm', [[<Cmd>set nomore<Bar>echo repeat("\n",&cmdheight)<Bar>40messages<Bar>set more<CR>]], {
+  noremap = true,
+})
+-- split line at current cursor position
+map('n', '<Leader>j', 'i<CR><Esc>k$', opts)
+
+map('n', '<M-q>', '<Cmd>lua require"ky.utils".toggle_quickfix()<CR>', opts)
+
+-- WARN: experimental
+map('o', ')', 't)', opts)
+map('o', '(', 't(', opts)
+map('x', ')', 't)', opts)
+map('x', '(', 't(', opts)
+
+-- @see https://github.com/mhinz/vim-galore#quickly-add-empty-lines
 map('n', '[<Space>', "<Cmd>put! =repeat(nr2char(10), v:count1)<CR>'[", opts)
 map('n', ']<Space>', '<Cmd>put =repeat(nr2char(10), v:count1)<CR>', opts)
 
-map('n', '<Leader>cd', '<Cmd>tcd %:p:h<Bar>pwd<CR>', opts)
-map('n', '<Leader>ud', '<Cmd>tcd ..<Bar>pwd<CR>', opts)
+map('n', '<Leader>e', [[<Cmd>execute 'edit .'<CR>]], { noremap = true })
+map('n', '-', [[<Cmd>execute 'edit ' .. expand('%:p:h')<CR>]], { noremap = true })
+
+map('n', '<Leader>cd', '<Cmd>tcd %:p:h <Bar> pwd<CR>', opts)
+map('n', '<Leader>ud', '<Cmd>tcd .. <Bar> pwd<CR>', opts)
 
 map('n', '<BS>', '<C-^>', opts)
 
 -- tab
-map('n', '<Leader>tn', '<Cmd>tabnew<CR>', opts)
-map('n', '<Leader>tc', '<Cmd>tabclose<CR>', opts)
 map('n', '<Leader>te', '<Cmd>tabedit %<CR>', opts)
+map('n', '<Leader>tC', '<Cmd>tabclose!<CR>', opts)
+map('n', '<Leader>tn', '<Cmd>tabnew<CR>', opts)
+map('n', '<Leader>to', '<Cmd>tabonly<CR>', opts)
+map('n', '<Leader>ts', '<Cmd>tab split<CR>', opts)
+map('n', '<Leader>ti', '<Cmd>tabs<CR>', opts)
+map('n', '<Leader>tl', [[<Cmd>execute 'tabmove ' . (v:count ? (v:count - 1) : '+1')<CR>]], opts)
+map('n', '<Leader>th', [[<Cmd>execute 'tabmove ' . (v:count ? (v:count - 1) : '-1')<CR>]], opts)
+
+map('i', '<M-o>', '<C-o>o', { noremap = true })
+map('i', '<M-O>', '<C-o>O', { noremap = true })
 
 -- WARN: experimental
 map('i', ',', ',<Space>', opts)
@@ -72,7 +140,8 @@ map('x', '<M-k>', ":m '<-2<CR>gv=gv", opts)
 map('n', 'J', 'mzJ`z', opts)
 
 -- Multiple Cursors
--- NOTE: http://www.kevinli.co/posts/2017-01-19-multiple-cursors-in-500-bytes-of-vimscript/
+-- TODO: convert to lua
+-- @see: http://www.kevinli.co/posts/2017-01-19-multiple-cursors-in-500-bytes-of-vimscript/
 vim.cmd [[
 let g:mc = "y/\\V\<C-r>=escape(@\", '/')\<CR>\<CR>"
 
