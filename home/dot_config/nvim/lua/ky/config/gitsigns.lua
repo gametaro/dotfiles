5999822b35749051e74b926a5fdc0f88997399aa
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2021-12-23 23:16:46 +0900

    chore(gitsigns): update config

--- home/dot_config/nvim/lua/ky/config/gitsigns.lua
+++ home/dot_config/nvim/lua/ky/config/gitsigns.lua
@@ -1,45 +1,4 @@
 local gitsigns = require 'gitsigns'
-local actions = require 'gitsigns.actions'
-
-local M = {}
-
-function M.stage_buffer()
-  actions.stage_buffer()
-  if pcall(require, 'diffview') then
-    vim.cmd 'DiffviewRefresh'
-  end
-end
-
-function M.stage_hank()
-  actions.stage_hunk()
-  if pcall(require, 'diffview') then
-    vim.cmd 'DiffviewRefresh'
-  end
-end
-
-function M.stage_hank_range()
-  actions.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
-  if pcall(require, 'diffview') then
-    vim.cmd 'DiffviewRefresh'
-  end
-end
-
-function M.undo_stage_hank()
-  actions.undo_stage_hunk()
-  if pcall(require, 'diffview') then
-    vim.cmd 'DiffviewRefresh'
-  end
-end
-
-function M.next_hunk()
-  actions.next_hunk()
-  actions.preview_hunk()
-end
-
-function M.prev_hunk()
-  actions.prev_hunk()
-  actions.preview_hunk()
-end
 
 gitsigns.setup {
   signs = {
@@ -49,32 +8,35 @@ gitsigns.setup {
     topdelete = { hl = 'GitSignsDelete', text = '', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
     changedelete = { hl = 'GitSignsChange', text = '▍', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
   },
+  signcolumn = true,
   numhl = false,
   linehl = false,
+  word_diff = true,
   keymaps = {
     -- Default keymap options
     noremap = true,
-    buffer = true,
-    ['n ]c'] = { expr = true, "&diff ? ']c' : '<Cmd>lua require\"ky.config.gitsigns\".next_hunk()<CR>'" },
-    ['n [c'] = { expr = true, "&diff ? '[c' : '<Cmd>lua require\"ky.config.gitsigns\".prev_hunk()<CR>'" },
-    ['n <LocalLeader>hb'] = '<Cmd>lua require"ky.config.gitsigns".stage_buffer()<CR>',
-    ['n <LocalLeader>hs'] = '<Cmd>lua require"ky.config.gitsigns".stage_hank()<CR>',
-    ['v <LocalLeader>hs'] = '<Cmd>lua require"ky.config.gitsigns".stage_hank_range()<CR>',
-    ['n <LocalLeader>hu'] = '<Cmd>lua require"ky.config.gitsigns".undo_stage_hank()<CR>',
+
+    ['n ]c'] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns.actions\".next_hunk()<CR>'" },
+    ['n [c'] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns.actions\".prev_hunk()<CR>'" },
+
+    ['n <LocalLeader>hs'] = '<Cmd>lua require"gitsigns".stage_hunk()<CR>',
+    ['x <LocalLeader>hs'] = '<Cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
+    ['n <LocalLeader>hu'] = '<Cmd>lua require"gitsigns".undo_stage_hank()<CR>',
     ['n <LocalLeader>hr'] = '<Cmd>lua require"gitsigns".reset_hunk()<CR>',
-    ['v <LocalLeader>hr'] = '<Cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
+    ['x <LocalLeader>hr'] = '<Cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
     ['n <LocalLeader>hR'] = '<Cmd>lua require"gitsigns".reset_buffer()<CR>',
     ['n <LocalLeader>hp'] = '<Cmd>lua require"gitsigns".preview_hunk()<CR>',
+    ['n <LocalLeader>hb'] = '<cmd>lua require"gitsigns".blame_line{full=true}<CR>',
     ['n <LocalLeader>hq'] = '<Cmd>lua require"gitsigns".setqflist("all")<CR>',
     ['n <LocalLeader>hl'] = '<Cmd>lua require"gitsigns".setloclist()<CR>',
+    ['n <LocalLeader>hS'] = '<Cmd>lua require"gitsigns".stage_buffer()<CR>',
+    ['n <LocalLeader>hU'] = '<cmd>lua require"gitsigns".reset_buffer_index()<CR>',
+
     ['o ih'] = '<Cmd>lua require"gitsigns.actions".select_hunk()<CR>',
     ['x ih'] = '<Cmd>lua require"gitsigns.actions".select_hunk()<CR>',
   },
+  current_line_blame = false,
   preview_config = {
     border = 'single',
   },
-  current_line_blame = false,
-  word_diff = true,
 }
-
-return M
