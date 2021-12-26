00e3d9cc21ba49e498429d9837df370b47337b0b
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2021-12-26 18:55:26 +0900

    chore(windline): remove unnecessary space

--- home/dot_config/nvim/lua/ky/config/windline.lua
+++ home/dot_config/nvim/lua/ky/config/windline.lua
@@ -119,7 +119,7 @@ basic.git = {
         { ' ', '' },
         { git_comps.diff_added { format = ' %s', show_zero = true }, 'green' },
         { git_comps.diff_removed { format = '  %s', show_zero = true }, 'red' },
-        { git_comps.diff_changed { format = ' 柳 %s', show_zero = true }, 'blue' },
+        { git_comps.diff_changed { format = ' 柳%s', show_zero = true }, 'blue' },
       }
     end
     return ''
