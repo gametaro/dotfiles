0b2cdd2305aa8f1955596d527eac8dde89408821
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2021-12-28 12:02:35 +0900

    feat(lsp): add `omnifunc` option

--- home/dot_config/nvim/lua/ky/lsp/init.lua
+++ home/dot_config/nvim/lua/ky/lsp/init.lua
@@ -33,6 +33,11 @@ function M.on_attach(client, bufnr)
   local function buf_set_keymap(...)
     vim.api.nvim_buf_set_keymap(bufnr, ...)
   end
+  local function buf_set_option(...)
+    vim.api.nvim_buf_set_option(bufnr, ...)
+  end
+
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
   local opts = { noremap = true, silent = true }
 
