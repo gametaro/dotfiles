cdde2a63eef934590d6ea8397d8c62f86e181cc9
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-01-01 22:48:09 +0900

    feat(telescope): add `cd` mappings

--- home/dot_config/nvim/lua/ky/config/telescope.lua
+++ home/dot_config/nvim/lua/ky/config/telescope.lua
@@ -91,6 +91,16 @@ telescope.setup {
     },
     find_files = {
       hidden = true,
+      mappings = {
+        n = {
+          ['cd'] = function(prompt_bufnr)
+            local selection = require('telescope.actions.state').get_selected_entry()
+            local dir = vim.fn.fnamemodify(selection.path, ':p:h')
+            require('telescope.actions').close(prompt_bufnr)
+            vim.cmd(string.format('silent tcd %s', dir))
+          end,
+        },
+      },
     },
   },
 }
