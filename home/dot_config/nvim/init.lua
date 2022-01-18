5a6bf1139a55dcfaed882d869d87c98672ebe437
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-01-19 00:11:13 +0900

    chore(filetype): add filetype.lua
    See https://github.com/neovim/neovim/issues/14090#issuecomment-1014888611

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -3,9 +3,6 @@ pcall(require, 'impatient')
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ','
 
-vim.g.do_filetype_lua = 1
-vim.g.did_load_filetypes = 0
-
 require 'ky.disable'
 require 'ky.plugins'
 require('ky.theme').setup 'nightfox'
