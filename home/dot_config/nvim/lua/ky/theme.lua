5a2f908108db0c5c15d687f2d967d422a202a106
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-01-18 20:23:59 +0900

    chore(nightfox): update config

--- home/dot_config/nvim/lua/ky/theme.lua
+++ home/dot_config/nvim/lua/ky/theme.lua
@@ -24,17 +24,8 @@ function M.setup(theme)
     elseif theme == 'nightfox' then
       local nightfox = require 'nightfox'
       nightfox.setup {
-        fox = 'duskfox',
-        hlgroups = {
-          TSVariable = { fg = '${fg_alt}' },
-          TelescopeNormal = { bg = '${bg_float}' },
-          TelescopePromptBorder = { fg = '#35334f', bg = '#35334f' },
-          TelescopeResultsBorder = { fg = '${bg_float}', bg = '${bg_float}' },
-          TelescopePreviewBorder = { fg = '${bg_float}', bg = '${bg_float}' },
-          TelescopePromptNormal = { bg = '#35334f' },
-          TelescopeResultsTitle = { fg = '${bg_float}', bg = '${bg_float}' },
-          TelescopePreviewTitle = { fg = '${bg_float}', bg = '${bg_float}' },
-        },
+        fox = 'nightfox',
+        alt_nc = true,
       }
       nightfox.load()
     end
