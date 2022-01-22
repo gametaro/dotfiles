28cf8c2d8fbe6d200bf915ebb9ed98daa295a867
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-01-22 11:25:38 +0900

    chore(chezmoi): use .profile to set env var

--- home/dot_config/private_fish/config.fish
+++ home/dot_config/private_fish/config.fish
@@ -1,22 +1,10 @@
 set fish_greeting
 
-if command -q nvim
-    set -gx EDITOR (which nvim)
-    set -gx VISUAL (which nvim)
-end
-
-fish_add_path ~/.local/bin
-
 if test -d ~/.asdf
     source ~/.asdf/asdf.fish
     if not test -L ~/.config/fish/completions/asdf.fish
         mkdir -p ~/.config/fish/completions; and ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
     end
-    set -q ASDF_CONFIG_FILE; or set -gx ASDF_CONFIG_FILE ~/.config/asdf/config
-end
-
-if command -q rg
-    set -q RIPGREP_CONFIG_PATH; or set -gx RIPGREP_CONFIG_PATH ~/.config/ripgrep/config
 end
 
 if command -q chezmoi
@@ -48,7 +36,6 @@ if status --is-interactive
 end
 
 if command -q starship
-    set -q STARSHIP_CONFIG; or set -gx STARSHIP_CONFIG ~/.config/starship/starship.toml
     starship init fish | source
 end
 
