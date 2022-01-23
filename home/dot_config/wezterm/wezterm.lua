ab958f219c63504be9bdac883dcdf78f04b9052c
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-01-23 13:18:58 +0900

    chore(wezterm): update config
    - do not set default_prog (use clink autorun instead)
    - add `pwsh` to `launch_menu`
    - disable `default_gui_startup_args`
    - disable ligature on FiraCode

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -1,13 +1,17 @@
 local wezterm = require 'wezterm'
 
 local launch_menu = {}
-local default_prog;
 local set_environment_variables = {}
 
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
   table.insert(launch_menu, {
     label = 'PowerShell',
     args = { 'powershell.exe', '-NoLogo' },
+  })
+
+  table.insert(launch_menu, {
+    label = 'Pwsh',
+    args = { 'pwsh.exe', '-NoLogo' },
   })
 
   table.insert(launch_menu, {
@@ -19,25 +23,19 @@ if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
   set_environment_variables["prompt"] = "$E]7;file://localhost/$P$E\\$E[32m$T$E[0m $E[35m$P$E[36m$_$G$E[0m "
   -- use a more ls-like output format for dir
   set_environment_variables["DIRCMD"] = "/d"
-  -- And inject clink into the command prompt
-  default_prog = {"cmd.exe", "/s", "/k", "%USERPROFILE%/scoop/shims/clink", "inject", "-q"}
 end
 
 return {
-  unix_domains = {
-    {
-      name = 'unix',
-    },
-  },
-  default_gui_startup_args = { 'connect', 'unix' },
-  default_prog = default_prog,
   set_environment_variables = set_environment_variables,
+  launch_menu = launch_menu,
   use_ime = true,
   enable_tab_bar = true,
-  font = wezterm.font_with_fallback { 'FiraCode NF', 'Consolas' },
+  font = wezterm.font({
+    family='FiraCode NF',
+    harfbuzz_features={"calt=0", "clig=0", "liga=0"}
+  }),
   font_size = 8.0,
   color_scheme = 'nightfox',
-  launch_menu = launch_menu,
   add_wsl_distributions_to_launch_menu = false,
   initial_rows = 30,
   initial_cols = 100,
