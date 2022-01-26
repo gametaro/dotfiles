cd009bbbb30bffca4423f47e67a695ca81b5a61a
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-01-26 23:49:55 +0900

    chore(pwsh): remove trailing whitespace

--- home/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ home/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -45,10 +45,10 @@ if (Get-Command zoxide -Type Application -ErrorAction SilentlyContinue)
 {
   Invoke-Expression (& {
       $hook = if ($PSVersionTable.PSVersion.Major -lt 6)
-      { 
+      {
         'prompt'
       } else
-      { 
+      {
         'pwd'
       }
     (zoxide init --hook $hook powershell | Out-String)
