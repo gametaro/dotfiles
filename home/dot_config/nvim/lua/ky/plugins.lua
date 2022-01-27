2b2dddb2367fbf75d165b16e24b9114b5a3da2e8
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-01-27 21:07:59 +0900

    chore(plugins): remove `nvim-ts-autotag`
    Use LuaSnip instead

--- home/dot_config/nvim/lua/ky/plugins.lua
+++ home/dot_config/nvim/lua/ky/plugins.lua
@@ -141,7 +141,6 @@ local function plugins(use)
     requires = {
       { 'nvim-treesitter/nvim-treesitter-textobjects', after = 'nvim-treesitter' },
       { 'p00f/nvim-ts-rainbow', after = 'nvim-treesitter' },
-      { 'windwp/nvim-ts-autotag', after = 'nvim-treesitter' },
       { 'RRethy/nvim-treesitter-textsubjects', after = 'nvim-treesitter' },
       { 'JoosepAlviste/nvim-ts-context-commentstring', after = 'nvim-treesitter' },
       { 'nvim-treesitter/playground', after = 'nvim-treesitter' },
