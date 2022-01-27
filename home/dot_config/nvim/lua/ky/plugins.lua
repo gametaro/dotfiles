e514448b0714972f46dd3c4bfa1562be2fdeec31
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-01-28 00:12:45 +0900

    feat(plugins): add `vim-projectionist`

--- home/dot_config/nvim/lua/ky/plugins.lua
+++ home/dot_config/nvim/lua/ky/plugins.lua
@@ -723,6 +723,35 @@ local function plugins(use)
       }
     end,
   }
+
+  use {
+    'tpope/vim-projectionist',
+    setup = function()
+      vim.g.projectionist_heuristics = {
+        ['lua/*.lua'] = {
+          ['*.lua'] = { alternate = { '{}_spec.lua' }, type = 'source' },
+          ['*_spec.lua'] = { alternate = '{}.lua', type = 'test' },
+        },
+        ['*.ts'] = {
+          ['*.ts'] = { alternate = { '{}.test.ts', '{}.spec.ts' }, type = 'source' },
+          ['*.test.ts'] = { alternate = '{}.ts', type = 'test' },
+          ['*.spec.ts'] = { alternate = '{}.ts', type = 'test' },
+        },
+        ['*.tsx'] = {
+          ['*.tsx'] = { alternate = { '{}.test.tsx', '{}.spec.tsx' }, type = 'source' },
+          ['*.test.tsx'] = { alternate = '{}.tsx', type = 'test' },
+          ['*.spec.tsx'] = { alternate = '{}.tsx', type = 'test' },
+        },
+        ['*.py'] = {
+          ['*.py'] = { alternate = { 'test_{}.py' }, type = 'source' },
+          ['test_*.py'] = { alternate = { '{}.py' }, type = 'test' },
+        },
+      }
+      vim.keymap.set('n', '<LocalLeader>a', '<Cmd>A<CR>')
+      vim.keymap.set('n', '<LocalLeader>Av', '<Cmd>AV<CR>')
+      vim.keymap.set('n', '<LocalLeader>As', '<Cmd>AS<CR>')
+    end,
+  }
 end
 
 bootstrap()
