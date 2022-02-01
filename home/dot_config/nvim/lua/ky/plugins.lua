7273221e9abc55378a1b9ba7063530597ad8c364
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-02-01 22:58:20 +0900

    chore(swap): disable default mappings

--- home/dot_config/nvim/lua/ky/plugins.lua
+++ home/dot_config/nvim/lua/ky/plugins.lua
@@ -40,6 +40,10 @@ local function plugins(use)
     'machakann/vim-swap',
     event = { 'BufRead' },
     setup = function()
+      vim.g.swap_no_default_key_mappings = 1
+
+      vim.keymap.set('n', 'g<', '<Plug>(swap-prev)')
+      vim.keymap.set('n', 'g>', '<Plug>(swap-next)')
       vim.keymap.set({ 'o', 'x' }, 'i,', '<Plug>(swap-textobject-i)')
       vim.keymap.set({ 'o', 'x' }, 'a,', '<Plug>(swap-textobject-a)')
     end,
