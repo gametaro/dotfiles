cd8bdaa7d22fde896fb19066c1d5f361f0973eaf
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-02-14 22:39:20 +0900

    refactor(lsp): move builtin lsp config to plugin dir

--- home/dot_config/nvim/lua/ky/lsp/init.lua
+++ home/dot_config/nvim/lua/ky/lsp/init.lua
@@ -1,47 +1,7 @@
 local lsp_installer = require('nvim-lsp-installer')
 local cmp_nvim_lsp = require('cmp_nvim_lsp')
-local icons = require('ky.theme').icons
-
-local lsp = vim.lsp
 
 local M = {}
-
-local float = {
-  border = require('ky.theme').border,
-  source = 'always',
-}
-
-vim.diagnostic.config {
-  severity_sort = true,
-  virtual_text = false,
-  float = float,
-}
-
-lsp.handlers['textDocument/hover'] = lsp.with(lsp.handlers.hover, float)
-lsp.handlers['textDocument/signatureHelp'] = lsp.with(lsp.handlers.signature_help, float)
--- https://github.com/neovim/nvim-lspconfig/wiki/User-contributed-tips#use-nvim-notify-to-display-lsp-messages
-lsp.handlers['window/showMessage'] = function(_, result, ctx)
-  local client = vim.lsp.get_client_by_id(ctx.client_id)
-  local level = ({
-    'ERROR',
-    'WARN',
-    'INFO',
-    'DEBUG',
-  })[result.type]
-  vim.notify({ result.message }, level, {
-    title = 'LSP | ' .. client.name,
-    timeout = 10000,
-    keep = function()
-      return level == 'ERROR' or level == 'WARN'
-    end,
-  })
-end
-
-local signs = { Error = icons.error, Warn = icons.warn, Hint = icons.hint, Info = icons.info }
-for type, icon in pairs(signs) do
-  local hl = 'DiagnosticSign' .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
---
+++ home/dot_config/nvim/plugin/lsp.lua
@@ -0,0 +1,43 @@
+local icons = require('ky.theme').icons
+
+local lsp = vim.lsp
+
+local float = {
+  border = require('ky.theme').border,
+  source = 'always',
+}
+
+local signs = { Error = icons.error, Warn = icons.warn, Hint = icons.hint, Info = icons.info }
+for type, icon in pairs(signs) do
+  local hl = 'DiagnosticSign' .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+vim.diagnostic.config {
+  severity_sort = true,
+  virtual_text = {
+    source = 'always',
+    prefix = '',
+  },
+  float = float,
+}
+
+lsp.handlers['textDocument/hover'] = lsp.with(lsp.handlers.hover, float)
+lsp.handlers['textDocument/signatureHelp'] = lsp.with(lsp.handlers.signature_help, float)
+-- https://github.com/neovim/nvim-lspconfig/wiki/User-contributed-tips#use-nvim-notify-to-display-lsp-messages
+lsp.handlers['window/showMessage'] = function(_, result, ctx)
+  local client = vim.lsp.get_client_by_id(ctx.client_id)
+  local level = ({
+    'ERROR',
+    'WARN',
+    'INFO',
+    'DEBUG',
+  })[result.type]
+  vim.notify({ result.message }, level, {
+    title = 'LSP | ' .. client.name,
+    timeout = 10000,
+    keep = function()
+      return level == 'ERROR' or level == 'WARN'
+    end,
+  })
+end
