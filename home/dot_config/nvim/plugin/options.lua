67be905c4bd4b2145e6c9b7fdd9910fd42d160a2
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-02-20 00:29:50 +0900

    refactor(quickfix): simplify code

--- home/dot_config/nvim/plugin/options.lua
+++ home/dot_config/nvim/plugin/options.lua
@@ -1,4 +1,6 @@
 local g, opt = vim.g, vim.opt
+local fn = vim.fn
+
 local indent = 2
 
 opt.autowriteall = true
@@ -44,6 +46,7 @@ opt.listchars = {
 -- opt.mouse = 'a'
 opt.number = true
 opt.pumheight = 10
+opt.quickfixtextfunc = '{info -> v:lua._G.qftf(info)}'
 opt.relativenumber = true
 opt.shada = "!,'0,f0,<50,s10,h"
 opt.scrolloff = 5
@@ -83,61 +86,16 @@ opt.wildignorecase = true
 opt.wildoptions = 'pum'
 opt.wrap = false
 
--- see https://github.com/kevinhwang91/nvim-bqf#customize-quickfix-window-easter-egg
-function _G.qftf(info)
-  local items
-  local ret = {}
-  if info.quickfix == 1 then
-    items = fn.getqflist({ id = info.id, items = 0 }).items
-  else
-    items = fn.getloclist(info.winid, { id = info.id, items = 0 }).items
-  end
-  local limit = 31
-  local fname_fmt1, fname_fmt2 = '%-' .. limit .. 's', '…%.' .. (limit - 1) .. 's'
-  local valid_fmt = '%s │%5d:%-3d│%s %s'
-  for i = info.start_idx, info.end_idx do
-    local e = items[i]
-    local fname = ''
-    local str
-    if e.valid == 1 then
-      if e.bufnr > 0 then
-        fname = fn.bufname(e.bufnr)
-        if fname == '' then
-          fname = '[No Name]'
-        else
-          fname = fname:gsub('^' .. vim.env.HOME, '~')
-        end
-        -- char in fname may occur more than 1 width, ignore this issue in order to keep performance
-        if #fname <= limit then
-          fname = fname_fmt1:format(fname)
-        else
-          fname = fname_fmt2:format(fname:sub(1 - limit))
-        end
-      end
-      local lnum = e.lnum > 99999 and -1 or e.lnum
-      local col = e.col > 999 and -1 or e.col
-      local qtype = e.type == '' and '' or ' ' .. e.type:sub(1, 1):upper()
-      str = valid_fmt:format(fname, lnum, col, qtype, e.text)
-    else
-      str = e.text
-    end
-    table.insert(ret, str)
-  end
-  return ret
-end
-
-vim.opt.quickfixtextfunc = '{info -> v:lua._G.qftf(info)}'
-
-if vim.fn.executable('fish') > 0 then
+if fn.executable('fish') > 0 then
   opt.shell = '/usr/bin/fish'
 end
 
-if vim.fn.executable('rg') > 0 then
+if fn.executable('rg') > 0 then
   opt.grepprg = 'rg --vimgrep --smart-case --hidden --glob=!.git'
 end
 
 -- neovide
-if vim.fn.exists('g:neovide') > 0 then
+if fn.exists('g:neovide') > 0 then
   g.neovide_cursor_animation_length = 0
   g.neovide_cursor_trail_length = 0
   opt.guifont = 'FiraCode NF:style=Regular:h11'
---
+++ home/dot_config/nvim/plugin/quickfix.lua
@@ -0,0 +1,31 @@
+local fn = vim.fn
+
+local items = function(info)
+  return info.quickfix == 1 and fn.getqflist({ id = info.id, items = 0 }).items
+    or fn.getloclist(info.winid, { id = info.id, items = 0 }).items
+end
+
+local format_fname = function(fname, limit)
+  fname = fname == '' and '[No Name]' or fn.fnamemodify(fname, ':p:.')
+  fname = fname:gsub('^' .. vim.env.HOME, '~')
+  limit = vim.F.if_nil(limit, 31)
+
+  local fname_fmt1 = '%-' .. limit .. 's'
+  local fname_fmt2 = '…%.' .. (limit - 1) .. 's'
+
+  -- char in fname may occur more than 1 width, ignore this issue in order to keep performance
+  return #fname <= limit and fname_fmt1:format(fname) or fname_fmt2:format(fname:sub(1 - limit))
+end
+
+function _G.qftf(info)
+  return vim.tbl_map(function(item)
+    return item.valid ~= 1 and item.text
+      or (function()
+        local fname = item.bufnr > 0 and format_fname(fn.bufname(item.bufnr)) or ''
+        local lnum = item.lnum > 99999 and -1 or item.lnum
+        local col = item.col > 999 and -1 or item.col
+        local qtype = item.type == '' and '' or ' ' .. item.type:sub(1, 1):upper()
+        return string.format('%s │%5d:%-3d│%s %s', fname, lnum, col, qtype, item.text)
+      end)()
+  end, items(info))
+end
