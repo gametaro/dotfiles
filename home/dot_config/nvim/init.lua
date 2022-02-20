9eb53f9e48b03021af5adb98577b0889a78f214a
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-02-20 17:47:42 +0900

    chore(impatient): enable profile

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -4,8 +4,9 @@ vim.g.maplocalleader = ','
 vim.g.do_filetype_lua = 1
 vim.g.did_load_filetypes = 0
 
-pcall(require, 'impatient')
+pcall(function()
+  require('impatient').enable_profile()
+end)
 require('ky.disable')
 require('ky.plugins')
 require('ky.theme').setup('vscode')
-pcall(require, 'packer_compiled')
