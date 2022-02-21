78f72de0315ec2f0b9e7bf57f94782a8eb3cdf57
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-02-22 01:06:06 +0900

    chore(autopairs): remove unused code

--- home/dot_config/nvim/lua/ky/config/autopairs.lua
+++ home/dot_config/nvim/lua/ky/config/autopairs.lua
@@ -16,7 +16,6 @@ local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 require('cmp').event:on('confirm_done', cmp_autopairs.on_confirm_done { map_char = { tex = '' } })
 
 local Rule = require('nvim-autopairs.rule')
-local cond = require('nvim-autopairs.conds')
 
 npairs.add_rules {
   -- add spaces between parentheses
@@ -49,34 +48,4 @@ npairs.add_rules {
       return opts.prev_char:match('.%]') ~= nil
     end)
     :use_key(']'),
-  -- add spaces on f=
-  -- see https://github.com/windwp/nvim-autopairs/wiki/Custom-rules#auto-addspace-on-
-  Rule('=', '')
-    :with_pair(cond.not_inside_quote())
-    :with_pair(cond.not_filetypes { 'sh', 'html' })
-    :with_pair(function(opts)
-      local last_char = opts.line:sub(opts.col - 1, opts.col - 1)
-      if last_char:match('[%w%=%s]') then
-        return true
-      end
-      return false
-    end)
-    :replace_endpair(function(opts)
-      local prev_2char = opts.line:sub(opts.col - 2, opts.col - 1)
-      local next_char = opts.line:sub(opts.col, opts.col)
-      next_char = next_char == ' ' and '' or ' '
-      if prev_2char:match('%w$') then
-        return '<bs> =' .. next_char
-      end
-      if prev_2char:match('%=$') then
-        return next_char
-      end
-      if prev_2char:match('=') then
-        return '<bs><bs>=' .. next_char
-      end
-      return ''
-    end)
-    :set_end_pair_length(0)
-    :with_move(cond.none())
-    :with_del(cond.none()),
 }
