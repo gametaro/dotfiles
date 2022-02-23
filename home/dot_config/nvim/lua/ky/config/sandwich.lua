e5777d30743eabc1fbcbcac2b243c22c91ae3aa0
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-02-23 12:49:51 +0900

    refactor(sandwich): do not use vim.cmd as much as possible

--- home/dot_config/nvim/lua/ky/config/sandwich.lua
+++ home/dot_config/nvim/lua/ky/config/sandwich.lua
@@ -4,31 +4,79 @@ local fmt = string.format
 vim.cmd([[autocmd User OperatrSandwichAddPre,OperatorSandwichReplacePre NoMatchParen]])
 vim.cmd([[autocmd User OperatorSandwichAddPost,OperatorSandwichReplacePost DoMatchParen]])
 
-vim.cmd([=[
-      augroup mine_sandwich
-        autocmd!
-        autocmd FileType python call sandwich#util#addlocal([
-          \   {'buns': ['"""', '"""'], 'nesting': 0, 'input': ['3"']},
-          \ ])
-        autocmd FileType sh call sandwich#util#addlocal([
-          \   {'buns': ['$(', ')'], 'nesting': 0, 'input': ['$(']},
-          \   {'buns': ['"$', '"'], 'nesting': 0, 'input': ['$"']},
-          \   {'buns': ['${', '}'], 'nesting': 0, 'input': ['${']},
-          \   {'buns': ['[[', ']]'], 'nesting': 0, 'input': ['2[', '2]']},
-          \ ])
-        autocmd FileType lua call sandwich#util#addlocal([
-          \   {'buns': ['[[', ']]'], 'nesting': 0, 'input': ['2[', '2]']},
-          \ ])
-        autocmd FileType javascript,typescript,javascriptreact,typescriptreact call sandwich#util#addlocal([
-          \   {'buns': ['${', '}'], 'nesting': 0, 'input': ['$']},
-          \ ])
-        autocmd FileType markdown call sandwich#util#addlocal([
-          \   {'buns': ['```\a*', '```'], 'nesting': 0, 'regex': 1, 'input': ['3`']},
-          \   {'buns': ['^\(#\{1,6}\s+.\+\)', '^\(#\{1,6}\s+.\+\)'], 'nesting': 0, 'regex': 1, 'input': ['#']},
-          \   {'buns': ['^\(\s*\(*\|-\)\s\+\)', '\n'], 'nesting': 0, 'regex': 1, 'input': ['-']},
-          \ ])
-      augroup END
-      ]=])
+-- keep cursor position
+vim.fn['operator#sandwich#set']('all', 'all', 'cursor', 'keep')
+-- do not highlight target when operating delete action
+vim.fn['operator#sandwich#set']('delete', 'all', 'highlight', 0)
+vim.fn['textobj#sandwich#set']('all', 'skip_break', 1)
+
+vim.g['sandwich#recipes'] = vim.tbl_extend('force', vim.g['sandwich#default_recipes'], {
+  {
+    buns = { '**', '**' },
+    nesting = 0,
+    filetype = { 'markdown' },
+    input = { '2*' },
+  },
+  {
+    buns = { '***', '***' },
+    nesting = 0,
+    filetype = { 'markdown' },
+    input = { '3*' },
+  },
+  {
+    buns = { '```\a*', '```' },
+    nesting = 0,
+    regex = 1,
+    filetype = { 'markdown' },
+    input = { '3`' },
+  },
+  {
+    buns = { [[^\(#\{1,6}\s+.\+\)]], [[^\(#\{1,6}\s+.\+\)]] },
+    nesting = 0,
+    regex = 1,
+    filetype = { 'markdown' },
+    input = {
+      '#',
+    },
+  },
+  {
+    buns = { [[^\(\s*\(*\|-\)\s\+\)]], '\n' },
+    nesting = 0,
+    regex = 1,
+    filetype = { 'markdown' },
+    input = { '-' },
+  },
+  {
+    buns = { '"""', '"""' },
+    nesting = 0,
+    filetype = { 'python' },
+    input = { '3"' },
+  },
+  {
+    buns = { '$(', ')' },
+    nesting = 0,
+    filetype = { 'sh' },
+    input = { '$(' },
+  },
+  {
+    buns = { '"$', '"' },
+    nesting = 0,
+    filetype = { 'sh' },
+    input = { '$"' },
+  },
+  {
+    buns = { '${', '}' },
+    nesting = 0,
+    filetype = { 'sh', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact' },
+    input = { '${' },
+  },
+  {
+    buns = { '[[', ']]' },
+    nesting = 0,
+    filetype = { 'sh', 'lua' },
+    input = { '2[', '2]' },
+  },
+})
 
 local map = vim.keymap.set
 map({ 'n', 'x' }, 's', '<Nop>')
@@ -41,9 +89,3 @@ end
 
 map({ 'o', 'x' }, 'im', '<Plug>(textobj-sandwich-literal-query-i)')
 map({ 'o', 'x' }, 'am', '<Plug>(textobj-sandwich-literal-query-a)')
-
--- keep cursor position
-vim.fn['operator#sandwich#set']('all', 'all', 'cursor', 'keep')
--- do not highlight target when operating delete action
-vim.fn['operator#sandwich#set']('delete', 'all', 'highlight', 0)
-vim.fn['textobj#sandwich#set']('all', 'skip_break', 1)
