a807c1d87e76bfec8acd94c4a70ba93edfcde8a5
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-02-26 12:11:41 +0900

    chore(sandwich): remove conflicting recipes

--- home/dot_config/nvim/lua/ky/config/sandwich.lua
+++ home/dot_config/nvim/lua/ky/config/sandwich.lua
@@ -10,73 +10,48 @@ vim.fn['operator#sandwich#set']('all', 'all', 'cursor', 'keep')
 vim.fn['operator#sandwich#set']('delete', 'all', 'highlight', 0)
 vim.fn['textobj#sandwich#set']('all', 'skip_break', 1)
 
-vim.g['sandwich#recipes'] = vim.tbl_extend('force', vim.g['sandwich#default_recipes'], {
-  {
-    buns = { '**', '**' },
-    nesting = 0,
-    filetype = { 'markdown' },
-    input = { '2*' },
-  },
-  {
-    buns = { '***', '***' },
-    nesting = 0,
-    filetype = { 'markdown' },
-    input = { '3*' },
-  },
-  {
-    buns = { '```\a*', '```' },
-    nesting = 0,
-    regex = 1,
-    filetype = { 'markdown' },
-    input = { '3`' },
-  },
-  {
-    buns = { [[^\(#\{1,6}\s+.\+\)]], [[^\(#\{1,6}\s+.\+\)]] },
-    nesting = 0,
-    regex = 1,
-    filetype = { 'markdown' },
-    input = {
-      '#',
-    },
-  },
-  {
-    buns = { [[^\(\s*\(*\|-\)\s\+\)]], '\n' },
-    nesting = 0,
-    regex = 1,
-    filetype = { 'markdown' },
-    input = { '-' },
-  },
-  {
-    buns = { '"""', '"""' },
-    nesting = 0,
-    filetype = { 'python' },
-    input = { '3"' },
-  },
-  {
-    buns = { '$(', ')' },
-    nesting = 0,
-    filetype = { 'sh' },
-    input = { '$(' },
-  },
-  {
-    buns = { '"$', '"' },
-    nesting = 0,
-    filetype = { 'sh' },
-    input = { '$"' },
-  },
-  {
-    buns = { '${', '}' },
-    nesting = 0,
-    filetype = { 'sh', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact' },
-    input = { '${' },
-  },
-  {
-    buns = { '[[', ']]' },
-    nesting = 0,
-    filetype = { 'sh', 'lua' },
-    input = { '2[', '2]' },
-  },
-})
+vim.g['sandwich#recipes'] =
+  vim.tbl_extend(
+    'force',
+    vim.deepcopy(vim.g['sandwich#default_recipes']),
+    {
+      {
+        buns = { '${', '}' },
+        filetype = {
+          'sh',
+          'javascript',
+          'typescript',
+          'javascriptreact',
+          'typescriptreact',
+        },
+        input = { '$' },
+      },
+      {
+        buns = { '**', '**' },
+        nesting = 0,
+        filetype = { 'markdown' },
+        input = { '2*' },
+      },
+      {
+        buns = { '***', '***' },
+        nesting = 0,
+        filetype = { 'markdown' },
+        input = { '3*' },
+      },
+      {
+        buns = { '"""', '"""' },
+        nesting = 0,
+        filetype = { 'python' },
+        input = { '3"' },
+      },
+      {
+        buns = { '$(', ')' },
+        nesting = 0,
+        filetype = { 'sh' },
+        input = { '$(' },
+      },
+    }
+  )
 
 local map = vim.keymap.set
 map({ 'n', 'x' }, 's', '<Nop>')
