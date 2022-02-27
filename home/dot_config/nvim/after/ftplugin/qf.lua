ed5d2069024986bc5a0ec1b1a75dd331df7dd171
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-02-27 23:16:25 +0900

    chore(ftplugin): enable `number` for quickfix

--- home/dot_config/nvim/after/ftplugin/qf.lua
+++ home/dot_config/nvim/after/ftplugin/qf.lua
@@ -1,6 +1,6 @@
 vim.opt_local.buflisted = false
 vim.opt_local.list = false
-vim.opt_local.number = false
+vim.opt_local.number = true
 vim.opt_local.relativenumber = false
 
 vim.keymap.set('n', 'cc', function()
