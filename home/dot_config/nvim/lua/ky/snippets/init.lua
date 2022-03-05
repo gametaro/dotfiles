401ea7cc69450e90cfde6c12c0c340e5d2164ef8
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-03-05 12:06:27 +0900

    chore(luasnip): add snippets for neogit

--- home/dot_config/nvim/lua/ky/snippets/init.lua
+++
@@ -1,9 +0,0 @@
-return {
-  all = require('ky.snippets.all'),
-  lua = require('ky.snippets.lua'),
-  html = require('ky.snippets.html'),
-  sh = require('ky.snippets.sh'),
-  gitcommit = require('ky.snippets.gitcommit'),
-  markdown = require('ky.snippets.markdown'),
-  typescript = require('ky.snippets.typescript'),
-}
