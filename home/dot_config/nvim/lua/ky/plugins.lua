02db582d2e5725cb80bf06eaaf80a0b72e301078
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-03-20 22:05:05 +0900

    chore(modes): use default config

--- home/dot_config/nvim/lua/ky/plugins.lua
+++ home/dot_config/nvim/lua/ky/plugins.lua
@@ -832,23 +832,7 @@ local plugins = function(use)
     event = { 'BufRead' },
     config = function()
       vim.opt.cursorline = true
-      require('modes').setup {
-        colors = {
-          copy = '#f5c359',
-          delete = '#c75c6a',
-          insert = '#78ccc5',
-          visual = '#9745be',
-        },
-
-        -- Cursorline highlight opacity
-        line_opacity = 0.1,
-
-        -- Highlight cursor
-        set_cursor = true,
-
-        -- Highlight in active window only
-        focus_only = true,
-      }
+      require('modes').setup {}
     end,
   }
 
