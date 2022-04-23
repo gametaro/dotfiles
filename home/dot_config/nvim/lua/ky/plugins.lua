669accc1d974fc995dd671118cf33eb7bbb2fd88
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-04-23 20:24:13 +0900

    feat(plugins): add indent-blankline.nvim

--- home/dot_config/nvim/lua/ky/plugins.lua
+++ home/dot_config/nvim/lua/ky/plugins.lua
@@ -969,6 +969,17 @@ local plugins = function(use)
       require('igit').setup()
     end,
   }
+
+  use {
+    'lukas-reineke/indent-blankline.nvim',
+    event = { 'BufRead' },
+    config = function()
+      require('indent_blankline').setup {
+        show_current_context = true,
+        show_current_context_start = true,
+      }
+    end,
+  }
 end
 
 bootstrap()
