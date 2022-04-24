7d152a2946417388259747067a2764617bc56939
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-04-24 23:47:33 +0900

    fix(ui): check if colorscheme exists

--- home/dot_config/nvim/lua/ky/ui.lua
+++ home/dot_config/nvim/lua/ky/ui.lua
@@ -11,36 +11,38 @@ M.border = 'single'
 
 M.colorscheme = 'nightfox'
 
-if
-  vim.tbl_contains({
-    'nightfox',
-    'duskfox',
-    'nordfox',
-    'dayfox',
-    'dawnfox',
-    'terafox',
-  }, M.colorscheme)
-then
-  require('nightfox').setup {
-    options = {
-      dim_inactive = true,
-    },
-    groups = {
-      NormalFloat = { fg = 'fg1', bg = 'bg0' },
-      FloatBorder = { fg = 'fg3', bg = 'bg0' },
-    },
-  }
-elseif M.colorscheme == 'kanagawa' then
-  require('kanagawa').setup {
-    commentStyle = 'NONE',
-    keywordStyle = 'NONE',
-    variablebuiltinStyle = 'NONE',
-  }
-elseif M.colorscheme == 'vscode' then
-  vim.g.vscode_style = 'dark'
-  vim.g.vscode_italic_comment = 0
-end
+if pcall(require, M.colorscheme) then
+  if
+    vim.tbl_contains({
+      'nightfox',
+      'duskfox',
+      'nordfox',
+      'dayfox',
+      'dawnfox',
+      'terafox',
+    }, M.colorscheme)
+  then
+    require('nightfox').setup {
+      options = {
+        dim_inactive = true,
+      },
+      groups = {
+        NormalFloat = { fg = 'fg1', bg = 'bg0' },
+        FloatBorder = { fg = 'fg3', bg = 'bg0' },
+      },
+    }
+  elseif M.colorscheme == 'kanagawa' then
+    require('kanagawa').setup {
+      commentStyle = 'NONE',
+      keywordStyle = 'NONE',
+      variablebuiltinStyle = 'NONE',
+    }
+  elseif M.colorscheme == 'vscode' then
+    vim.g.vscode_style = 'dark'
+    vim.g.vscode_italic_comment = 0
+  end
 
-vim.cmd('colorscheme ' .. M.colorscheme)
+  vim.cmd('colorscheme ' .. M.colorscheme)
+end
 
 return M
