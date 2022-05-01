ffacda78d4a4f0d4214efac264dad06f32a2a431
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-05-01 22:29:34 +0900

    chore(notify): set border to single

--- home/dot_config/nvim/lua/ky/config/notify.lua
+++ home/dot_config/nvim/lua/ky/config/notify.lua
@@ -3,6 +3,11 @@ local render = require('notify.render')
 
 notify.setup {
   timeout = 1000,
+  on_open = function(win)
+    if vim.api.nvim_win_is_valid(win) then
+      vim.api.nvim_win_set_config(win, { border = require('ky.ui').border })
+    end
+  end,
   render = function(bufnr, notif, highlights)
     local renderer = notif.title[1] == '' and 'minimal' or 'default'
     notif.keep = function()
