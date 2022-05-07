d64b69f6ff5c14faeb9b0c46af2aede5cee2228e
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-05-07 23:48:18 +0900

    fix(incline): remove unnecessary function

--- home/dot_config/nvim/lua/ky/config/incline.lua
+++ home/dot_config/nvim/lua/ky/config/incline.lua
@@ -1,34 +1,32 @@
-return function()
-  require('incline').setup {
-    hide = {
-      focused_win = true,
-      only_win = true,
+require('incline').setup {
+  hide = {
+    focused_win = true,
+    only_win = true,
+  },
+  ignore = {
+    filetypes = { 'qf' },
+  },
+  window = {
+    options = {
+      winblend = vim.o.winblend,
     },
-    ignore = {
-      filetypes = { 'qf' },
-    },
-    window = {
-      options = {
-        winblend = vim.o.winblend,
-      },
-    },
-    render = function(props)
-      local fname = vim.api.nvim_buf_get_name(props.buf)
-      fname = vim.fn.fnamemodify(fname, ':t')
-      if fname == '' then
-        fname = '[No Name]'
-      end
-      local extension = vim.fn.fnamemodify(fname, ':e')
-      local icon, color = require('nvim-web-devicons').get_icon_color(
-        fname,
-        extension,
-        { default = true }
-      )
-      local bg = string.format('#%06x', vim.api.nvim_get_hl_by_name('StatusLine', true).background)
-      return {
-        { icon .. ' ', guifg = color, guibg = bg },
-        { fname, guibg = bg, gui = 'bold' },
-      }
-    end,
-  }
-end
+  },
+  render = function(props)
+    local fname = vim.api.nvim_buf_get_name(props.buf)
+    fname = vim.fn.fnamemodify(fname, ':t')
+    if fname == '' then
+      fname = '[No Name]'
+    end
+    local extension = vim.fn.fnamemodify(fname, ':e')
+    local icon, color = require('nvim-web-devicons').get_icon_color(
+      fname,
+      extension,
+      { default = true }
+    )
+    local bg = string.format('#%06x', vim.api.nvim_get_hl_by_name('StatusLine', true).background)
+    return {
+      { icon .. ' ', guifg = color, guibg = bg },
+      { fname, guibg = bg, gui = 'bold' },
+    }
+  end,
+}
