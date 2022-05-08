9a80892624fa1510e58046c6a9c9169a996ba656
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-05-08 15:16:50 +0900

    style(auto-session): rename variables

--- home/dot_config/nvim/lua/ky/config/auto-session.lua
+++ home/dot_config/nvim/lua/ky/config/auto-session.lua
@@ -2,8 +2,8 @@ require('auto-session').setup {
   pre_save_cmds = {
     function()
       for _, win in ipairs(vim.api.nvim_list_wins()) do
-        local config_ = vim.api.nvim_win_get_config(win)
-        if config_.relative ~= '' then
+        local config = vim.api.nvim_win_get_config(win)
+        if config.relative ~= '' then
           vim.api.nvim_win_close(win, false)
         end
       end
