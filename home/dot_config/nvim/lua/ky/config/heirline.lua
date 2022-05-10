7fa230d46dc154eabe5fb7a2294dec75dbf2f7ba
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-05-10 21:34:23 +0900

    chore(heirline): change terminal icon

--- home/dot_config/nvim/lua/ky/config/heirline.lua
+++ home/dot_config/nvim/lua/ky/config/heirline.lua
@@ -414,7 +414,7 @@ local TerminalName = {
   end,
   provider = function()
     local tname, _ = vim.api.nvim_buf_get_name(0):gsub('.*:', '')
-    return ' ' .. tname
+    return ' ' .. tname
   end,
 }
 
