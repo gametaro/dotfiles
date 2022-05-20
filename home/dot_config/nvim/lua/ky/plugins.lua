1187575276946e30fa4b41be73e49d54238ee243
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-05-20 22:03:16 +0900

    chore(matchup): set offscreen method to popup

--- home/dot_config/nvim/lua/ky/plugins.lua
+++ home/dot_config/nvim/lua/ky/plugins.lua
@@ -817,6 +817,7 @@ local plugins = function(use)
     setup = function()
       vim.g.matchup_matchparen_deferred = 1
       vim.g.matchup_matchparen_deferred_hide_delay = 300
+      vim.g.matchup_matchparen_offscreen = { method = 'popup' }
     end,
   }
 
