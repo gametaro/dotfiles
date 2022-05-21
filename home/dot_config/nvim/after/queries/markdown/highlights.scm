5c23c42543c1e3d8734058980c9d42a354549d7b
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-05-21 19:42:36 +0900

    feat(markdown): add queries for highlights

---
+++ home/dot_config/nvim/after/queries/markdown/highlights.scm
@@ -0,0 +1,28 @@
+(inline_link
+  [
+    "("
+    ")"
+    (link_destination)
+  ] @conceal
+  (#set! conceal "")
+)
+
+(inline_link
+  (link_text
+    (image
+    [
+      (link_destination)
+      "("
+      ")"
+      "["
+      "]"
+      "!"
+    ] @conceal
+    )
+  )
+  (#set! conceal "")
+)
+
+(inline_link
+  (link_text) @text.underline
+)
