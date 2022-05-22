1d08627ef1c3ad82df6cdf99d96c3260a49fbe4a
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-05-22 17:51:26 +0900

    chore(project): set autocmd myself
    project.nvim sets cwd globally. (see :h nvim_set_current_dir())
    I wanna manage cwd per tabpage, so set autocmd which uses `tcd`
    command.

--- home/dot_config/nvim/lua/ky/plugins.lua
+++ home/dot_config/nvim/lua/ky/plugins.lua
@@ -557,10 +557,35 @@ local plugins = function(use)
         after = 'telescope.nvim',
         config = function()
           require('project_nvim').setup {
+            manual_mode = true,
             ignore_lsp = { 'null-ls' },
           }
           require('telescope').load_extension('projects')
           vim.keymap.set('n', '<LocalLeader>fp', '<Cmd>Telescope projects<CR>')
+
+          vim.api.nvim_create_autocmd({ 'BufEnter', 'VimEnter' }, {
+            callback = function()
+              if vim.tbl_contains({ 'nofile', 'prompt' }, vim.bo.buftype) then
+                return
+              end
+              if vim.tbl_contains({ 'help', 'qf' }, vim.bo.filetype) then
+                return
+              end
+              local ok, project = pcall(require, 'project_nvim.project')
+              if ok then
+                local root = project.get_project_root()
+                if root and vim.cmd('pwd') ~= root then
+                  vim.api.nvim_cmd({
+                    cmd = 'tcd',
+                    args = { root },
+                    mods = {
+                      silent = true,
+                    },
+                  }, {})
+                end
+              end
+            end,
+          })
         end,
       },
       {
