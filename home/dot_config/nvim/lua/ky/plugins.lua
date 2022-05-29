fff0faf41caa72731a63a6221a347fa4f3cd1b70
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-05-29 13:36:20 +0900

    refactor(project): move config into another file

--- home/dot_config/nvim/lua/ky/plugins.lua
+++ home/dot_config/nvim/lua/ky/plugins.lua
@@ -558,37 +558,7 @@ local plugins = function(use)
         'ahmedkhalf/project.nvim',
         after = 'telescope.nvim',
         config = function()
-          require('project_nvim').setup {
-            manual_mode = true,
-            ignore_lsp = { 'null-ls' },
-          }
-          require('telescope').load_extension('projects')
-          vim.keymap.set('n', '<LocalLeader>fp', '<Cmd>Telescope projects<CR>')
-
-          vim.api.nvim_create_autocmd({ 'BufEnter', 'VimEnter' }, {
-            group = vim.api.nvim_create_augroup('ProjectCd', { clear = true }),
-            callback = function()
-              if vim.tbl_contains({ 'nofile', 'prompt' }, vim.bo.buftype) then
-                return
-              end
-              if vim.tbl_contains({ 'help', 'qf' }, vim.bo.filetype) then
-                return
-              end
-              local ok, project = prequire('project_nvim.project')
-              if ok then
-                local root = project.get_project_root()
-                if root and vim.cmd('silent! pwd') ~= root then
-                  vim.api.nvim_cmd({
-                    cmd = 'tcd',
-                    args = { root },
-                    mods = {
-                      silent = true,
-                    },
-                  }, {})
-                end
-              end
-            end,
-          })
+          require('ky.config.project')
         end,
       },
       {
