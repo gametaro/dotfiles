a63ee1f2f39dba40402881f2db2e88890326c130
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-05-29 18:52:52 +0900

    chore(heirline): show long path instead of shorten one

--- home/dot_config/nvim/lua/ky/config/heirline.lua
+++ home/dot_config/nvim/lua/ky/config/heirline.lua
@@ -499,7 +499,7 @@ local LirName = {
   end,
   init = function(self)
     local dir = require('lir').get_context().dir
-    self.dir = vim.fn.fnamemodify(dir, ':.:h')
+    self.dir = vim.fn.fnamemodify(dir, ':~:h')
     self.icon, self.icon_color = require('nvim-web-devicons').get_icon_color('lir_folder_icon')
     self.show_hidden_files = require('lir.config').values.show_hidden_files
   end,
