793d4bff7f6c5edc3a9e9953c1ababbdb37ee265
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-06-12 00:46:25 +0900

    refactor(jump): much improved

--- home/dot_config/nvim/plugin/jump.lua
+++ home/dot_config/nvim/plugin/jump.lua
@@ -3,6 +3,9 @@
 -- 2. vim-EnhancedJumps
 -- 3. bufjump.nvim
 
+local api = vim.api
+local fn = vim.fn
+
 local config = {
   ignore_filetype = { 'gitcommit', 'gitrebase' },
   ignore_buftype = { 'terminal', 'help', 'quickfix', 'nofile' },
@@ -14,108 +17,89 @@ local config = {
 }
 
 local condition = function(bufnr)
-  if not vim.api.nvim_buf_is_valid(bufnr) then
+  if not api.nvim_buf_is_valid(bufnr) then
     return false
   end
   if
     config.ignore_buftype
-    and vim.tbl_contains(config.ignore_buftype, vim.api.nvim_buf_get_option(bufnr, 'buftype'))
+    and vim.tbl_contains(config.ignore_buftype, api.nvim_buf_get_option(bufnr, 'buftype'))
   then
     return false
   end
   if
     config.ignore_filetype
-    and vim.tbl_contains(config.ignore_filetype, vim.api.nvim_buf_get_option(bufnr, 'filetype'))
+    and vim.tbl_contains(config.ignore_filetype, api.nvim_buf_get_option(bufnr, 'filetype'))
   then
     return false
   end
-  if
-    config.only_cwd and not string.find(vim.api.nvim_buf_get_name(bufnr), vim.loop.cwd(), 1, true)
-  then
+  if config.only_cwd and not string.find(api.nvim_buf_get_name(bufnr), vim.loop.cwd(), 1, true) then
     return false
   end
   return true
 end
 
-local get_jumplist = function()
-  local winnr = vim.api.nvim_win_get_number(0)
-  local tabnr = vim.api.nvim_tabpage_get_number(0)
-  return unpack(vim.fn.getjumplist(winnr, tabnr))
-end
+local jump = function(opts)
+  local jumplist, current_pos = unpack(fn.getjumplist())
+  if vim.tbl_isempty(jumplist) then
+    return
+  end
 
-local get_pos = function(opts)
-  local curbufnr = vim.api.nvim_get_current_buf()
+  current_pos = current_pos + 1
+  if current_pos == (opts.forward and #jumplist or 1) then
+    return
+  end
 
-  local dstpos
-  local dstbufnr
-  local from = opts.forward and (opts.curpos + 1) or (opts.curpos - 1)
-  local to = opts.forward and #opts.jumplist or 1
+  local current_bufnr = api.nvim_get_current_buf()
+  local target_pos
+  local target_bufnr
+  local from = opts.forward and (current_pos + 1) or (current_pos - 1)
+  local to = opts.forward and #jumplist or 1
   local unit = opts.forward and 1 or -1
   for i = from, to, unit do
-    dstbufnr = opts.jumplist[i].bufnr
-    if opts.is_local and (dstbufnr == curbufnr) or (dstbufnr ~= curbufnr) then
-      if condition(dstbufnr) then
-        dstpos = i
+    target_bufnr = jumplist[i].bufnr
+    if opts.is_local and (target_bufnr == current_bufnr) or (target_bufnr ~= current_bufnr) then
+      if condition(target_bufnr) then
+        target_pos = i
         break
       end
     end
   end
 
-  return dstpos, dstbufnr
-end
-
-local jump = function(is_local, forward)
-  local jumplist, curpos = get_jumplist()
-  if #jumplist == 0 then
-    return
-  end
-
-  curpos = curpos + 1
-  if curpos == (forward and #jumplist or 1) then
-    return
-  end
-
-  local dstpos = get_pos {
-    is_local = is_local,
-    curpos = curpos,
-    forward = forward,
-    jumplist = jumplist,
-  }
-  if not dstpos then
-    config.on_error()
+  if target_pos == nil then
+    config.on_error(target_bufnr)
     return
   end
 
   vim.cmd(
     string.format(
       'execute "normal! %s%s"',
-      tostring(dstpos - curpos),
-      (forward and [[\<C-i>]] or [[\<c-o>]])
+      tostring(target_pos - current_pos),
+      (opts.forward and [[\<C-i>]] or [[\<C-o>]])
     )
   )
 
-  config.on_success()
+  config.on_success(target_bufnr)
 end
 
 local toqflist = function(jumplist)
   return vim.tbl_map(function(j)
-    local text = unpack(vim.api.nvim_buf_get_lines(j.bufnr, j.lnum - 1, j.lnum, true))
+    local text = unpack(api.nvim_buf_get_lines(j.bufnr, j.lnum - 1, j.lnum, true))
     return { bufnr = j.bufnr, col = j.col, lnum = j.lnum, text = text }
   end, jumplist)
 end
 
 local setlist = function(qf, open)
-  local jumplist = unpack(vim.fn.getjumplist())
+  local jumplist = unpack(fn.getjumplist())
   local items = toqflist(vim.tbl_filter(function(j)
-    return vim.api.nvim_buf_is_loaded(j.bufnr)
+    return api.nvim_buf_is_loaded(j.bufnr)
   end, jumplist))
   if qf then
-    vim.fn.setqflist({}, ' ', { title = 'Jumplist', items = items })
+    fn.setqflist({}, ' ', { title = 'Jumplist', items = items })
   else
-    vim.fn.setloclist(0, {}, ' ', { title = 'Jumplist', items = items })
+    fn.setloclist(0, {}, ' ', { title = 'Jumplist', items = items })
   end
   if open then
-    vim.api.nvim_command(qf and 'botright copen' or 'lopen')
+    api.nvim_command(qf and 'botright copen' or 'lopen')
   end
 end
 
@@ -127,20 +111,32 @@ local setloclist = function(open)
   setlist(false, open)
 end
 
-local forward = function()
-  jump(false, true)
+local forward = function(opts)
+  opts = opts or {}
+  opts.forward = true
+  opts.is_local = false
+  jump(opts)
 end
 
-local backward = function()
-  jump(false, false)
+local backward = function(opts)
+  opts = opts or {}
+  opts.forward = false
+  opts.is_local = false
+  jump(opts)
 end
 
-local forward_local = function()
-  jump(true, true)
+local forward_local = function(opts)
+  opts = opts or {}
+  opts.forward = true
+  opts.is_local = true
+  jump(opts)
 end
 
-local backward_local = function()
-  jump(true, false)
+local backward_local = function(opts)
+  opts = opts or {}
+  opts.forward = false
+  opts.is_local = true
+  jump(opts)
 end
 
 vim.keymap.set('n', '<Leader><C-i>', function()
