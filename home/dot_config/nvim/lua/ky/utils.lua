de7e9c74d380e22d01ef0931ac9baa04a681785f
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-06-15 23:26:33 +0900

    feat(utils): add utility function to toggle options

---
+++ home/dot_config/nvim/lua/ky/utils.lua
@@ -0,0 +1,29 @@
+local utils = {}
+
+---toggle vim options
+---@param name string
+---@param values table
+---@param opts table
+utils.toggle_options = function(name, values, opts)
+  opts = vim.tbl_extend('force', { silent = false }, opts or {})
+  values = values or vim.api.nvim_get_option_info(name).type == 'boolean' and { true, false }
+  local value
+  for i, v in ipairs(values) do
+    if vim.opt[name]:get() == v then
+      value = values[i == #values and 1 or i + 1]
+      vim.opt[name] = value
+      break
+    end
+  end
+  if not opts.silent then
+    vim.notify(string.format('`%s` was set to `%s`', name, value), 'info', {
+      title = debug.getinfo(1, 'n').name,
+      on_open = function(win)
+        local buf = vim.api.nvim_win_get_buf(win)
+        vim.api.nvim_buf_set_option(buf, 'filetype', 'markdown')
+      end,
+    })
+  end
+end
+
+return utils
