578d6ce8249f809476365d3dec69ec1080f7a917
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-06-22 15:03:20 +0900

    fix(mru): use correct command name

--- home/dot_config/nvim/after/plugin/scMRU.lua
+++ home/dot_config/nvim/after/plugin/scMRU.lua
@@ -1,3 +1,3 @@
 vim.keymap.set('n', '<LocalLeader><LocalLeader>', '<Cmd>Mru<CR>')
 vim.keymap.set('n', '<LocalLeader>.', '<Cmd>Mfu<CR>')
-vim.keymap.set('n', '<LocalLeader>m', '<Cmd>Mr<CR>')
+vim.keymap.set('n', '<LocalLeader>m', '<Cmd>MruRepos<CR>')
