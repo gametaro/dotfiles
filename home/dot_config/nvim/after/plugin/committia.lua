0ab88941a4b3a934980ed726384d68a3b231fd35
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-06-22 19:12:36 +0900

    feat(committia): start in insertmode

--- home/dot_config/nvim/after/plugin/committia.lua
+++ home/dot_config/nvim/after/plugin/committia.lua
@@ -1,5 +1,8 @@
 vim.g.committia_hooks = {
-  edit_open = function()
+  edit_open = function(info)
+    if info.vcs == 'git' and vim.fn.getline(1) == '' then
+      vim.cmd('startinsert')
+    end
     vim.keymap.set('i', '<A-d>', '<Plug>(committia-scroll-diff-down-half)', { buffer = true })
     vim.keymap.set('i', '<A-u>', '<Plug>(committia-scroll-diff-up-half)', { buffer = true })
   end,
