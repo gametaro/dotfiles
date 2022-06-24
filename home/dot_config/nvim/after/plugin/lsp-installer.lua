29a220e8bd66537875dae673752e016ff9052cd2
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-06-24 09:43:16 +0900

    feat(lsp-installer): set `ensure_installed`

--- home/dot_config/nvim/after/plugin/lsp-installer.lua
+++ home/dot_config/nvim/after/plugin/lsp-installer.lua
@@ -4,6 +4,18 @@ if not ok then
 end
 
 require('nvim-lsp-installer').setup {
+  ensure_installed = {
+    'bashls',
+    'cssls',
+    'dockerls',
+    'eslint',
+    'graphql',
+    'html',
+    'jsonls',
+    'sumneko_lua',
+    'tsserver',
+    'yamlls',
+  },
   ui = {
     border = require('ky.ui').border,
   },
