1393bda5ea92b0180ea735f53ada7b305562ddaa
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-06-26 14:54:28 +0900

    chore(todo-comments): remove unused mapping

--- home/dot_config/nvim/after/plugin/todo-comments.lua
+++ home/dot_config/nvim/after/plugin/todo-comments.lua
@@ -5,6 +5,5 @@ end
 
 vim.keymap.set('n', '<LocalLeader>tq', '<Cmd>TodoQuickFix<CR>')
 vim.keymap.set('n', '<LocalLeader>tl', '<Cmd>TodoLocList<CR>')
-vim.keymap.set('n', '<LocalLeader>tt', '<Cmd>TodoTrouble<CR>')
 
 require('todo-comments').setup()
