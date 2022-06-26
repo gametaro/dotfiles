afefde90b3f1059ac8db4352a6c18f4f5a917c71
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-06-26 23:52:46 +0900

    feat(mini): add mappings for <C-h>

--- home/dot_config/nvim/after/plugin/mini.lua
+++ home/dot_config/nvim/after/plugin/mini.lua
@@ -6,3 +6,10 @@ end
 require('mini.pairs').setup {
   modes = { insert = false, command = true, terminal = true },
 }
+
+vim.keymap.set(
+  { 'c', 't' },
+  '<C-h>',
+  'v:lua.MiniPairs.bs()',
+  { expr = true, desc = 'MiniPairs <BS>' }
+)
