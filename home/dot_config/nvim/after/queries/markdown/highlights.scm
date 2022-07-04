b3b1eadc86806125bc493414acd30385ff64bf9c
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-07-04 09:58:03 +0900

    chore(treesitter): temporarily disable my injection for markdown

--- home/dot_config/nvim/after/queries/markdown/highlights.scm
+++ home/dot_config/nvim/after/queries/markdown/highlights.scm
@@ -1,28 +1,28 @@
-(inline_link
-  [
-    "("
-    ")"
-    (link_destination)
-  ] @conceal
-  (#set! conceal "")
-)
-
-(inline_link
-  (link_text
-    (image
-    [
-      (link_destination)
-      "("
-      ")"
-      "["
-      "]"
-      "!"
-    ] @conceal
-    )
-  )
-  (#set! conceal "")
-)
-
-(inline_link
-  (link_text) @text.underline
-)
+; (inline_link
+;   [
+;     "("
+;     ")"
+;     (link_destination)
+;   ] @conceal
+;   (#set! conceal "")
+; )
+;
+; (inline_link
+;   (link_text
+;     (image
+;     [
+;       (link_destination)
+;       "("
+;       ")"
+;       "["
+;       "]"
+;       "!"
+;     ] @conceal
+;     )
+;   )
+;   (#set! conceal "")
+; )
+;
+; (inline_link
+;   (link_text) @text.underline
+; )
