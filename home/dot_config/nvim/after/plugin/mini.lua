d753fe2b830874171647f20ec02041db1a0ad553
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-07-14 09:47:07 +0900

    chore(mini): remove unused code

--- home/dot_config/nvim/after/plugin/mini.lua
+++ home/dot_config/nvim/after/plugin/mini.lua
@@ -64,10 +64,6 @@ require('mini.surround').setup {
 
 require('mini.jump').setup {
   mappings = {
-    -- forward = 'f',
-    -- backward = 'F',
-    -- forward_till = 't',
-    -- backward_till = 'T',
     repeat_jump = '',
   },
 }
