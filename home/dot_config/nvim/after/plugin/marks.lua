fe8e22e25505068479828029430319f5331bf481
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-07-15 23:21:41 +0900

    chore(marks): set `excluded_filetypes`

--- home/dot_config/nvim/after/plugin/marks.lua
+++ home/dot_config/nvim/after/plugin/marks.lua
@@ -3,5 +3,12 @@ if not ok then return end
 
 require('marks').setup {
   default_mappings = false,
-  builtin_marks = { '.', '<', '>', '^', "'", '"', '[', ']' },
+  builtin_marks = { '.', '^', "'", '"' },
+  excluded_filetypes = {
+    '',
+    'gitcommit',
+    'gitrebase',
+    'lspinfo',
+    'null-ls-info',
+  },
 }
