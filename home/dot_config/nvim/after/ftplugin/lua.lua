0a85448b1f47f7b3cdc5514714dd3491fbe3ddac
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-07-20 11:14:55 +0900

    chore(surround): replace deprecated function

--- home/dot_config/nvim/after/ftplugin/lua.lua
+++ home/dot_config/nvim/after/ftplugin/lua.lua
@@ -1,16 +1,19 @@
 local ok = prequire('nvim-surround')
 if not ok then return end
 
+---@param prompt? string
+local get_input = function(prompt)
+  local ok, result = pcall(vim.fn.input, { prompt = prompt })
+  if not ok then return nil end
+  return result
+end
+
 require('nvim-surround').buffer_setup {
   delimiters = {
     pairs = {
       ['f'] = function()
-        return {
-          'function '
-            .. require('nvim-surround.utils').get_input('Enter the function name: ')
-            .. '(',
-          ')',
-        }
+        local result = get_input('Enter the function name: ')
+        if result then return { result .. '(', ')' } end
       end,
     },
   },
