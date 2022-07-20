a8de1305e63c69e3913c13d1a99f15fe5592fd13
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-07-20 14:55:07 +0900

    chore(mapping): disable <CR> mapping
    It's conflict with autopairs's mapping

--- home/dot_config/nvim/plugin/mapping.lua
+++ home/dot_config/nvim/plugin/mapping.lua
@@ -89,7 +89,7 @@ map('i', '<C-a>', '<C-c>^')
 map('c', '<C-a>', '<Home>')
 map('!', '<C-e>', '<End>')
 map('i', '<C-]>', '<Esc><Right>')
-map('i', '<C-m>', '<C-g>u<C-m>')
+-- map('i', '<C-m>', '<C-g>u<C-m>')
 
 map('c', '<M-b>', '<S-Left>')
 map('c', '<M-f>', '<S-right>')
