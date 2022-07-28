d4ab1c5a0fc3255c864134c07f0337915fee8e10
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-07-29 00:35:12 +0900

    fix(pounce): correctly lazy loading

--- home/dot_config/nvim/after/plugin/pounce.lua
+++ home/dot_config/nvim/after/plugin/pounce.lua
@@ -1,5 +1,9 @@
-vim.keymap.set({ 'n', 'x' }, 's', '<Nop>')
-vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Cmd>Pounce<CR>')
-vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Cmd>PounceRepeat<CR>')
-
-require('pounce').setup {}
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'JetpackVimPouncePost',
+  callback = function()
+    vim.keymap.set({ 'n', 'x' }, 's', '<Nop>')
+    vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Cmd>Pounce<CR>')
+    vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Cmd>PounceRepeat<CR>')
+    require('pounce').setup {}
+  end,
+})
