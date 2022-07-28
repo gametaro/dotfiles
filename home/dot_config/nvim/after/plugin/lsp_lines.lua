a8024d1f04fda0d98b31189b171168777afbddea
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-07-28 15:55:35 +0900

    chore(lsp_lines): remove function wrap

--- home/dot_config/nvim/after/plugin/lsp_lines.lua
+++ home/dot_config/nvim/after/plugin/lsp_lines.lua
@@ -3,6 +3,4 @@ if not ok then return end
 
 require('lsp_lines').setup()
 
-vim.keymap.set('', '<LocalLeader>l', function()
-  require('lsp_lines').toggle()
-end, { desc = 'Toggle lsp_lines' })
+vim.keymap.set('', '<LocalLeader>l', require('lsp_lines').toggle, { desc = 'Toggle lsp_lines' })
