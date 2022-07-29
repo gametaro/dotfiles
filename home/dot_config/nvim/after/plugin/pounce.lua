9eedae846a08180481621fabbcab78d6933c6de9
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-07-29 10:30:14 +0900

    fix(pounce): set mapping outside autocmd

--- home/dot_config/nvim/after/plugin/pounce.lua
+++ home/dot_config/nvim/after/plugin/pounce.lua
@@ -1,9 +1,10 @@
+vim.keymap.set({ 'n', 'x' }, 's', '<Nop>')
+vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Cmd>Pounce<CR>')
+vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Cmd>PounceRepeat<CR>')
+
 vim.api.nvim_create_autocmd('User', {
   pattern = 'JetpackVimPouncePost',
   callback = function()
-    vim.keymap.set({ 'n', 'x' }, 's', '<Nop>')
-    vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Cmd>Pounce<CR>')
-    vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Cmd>PounceRepeat<CR>')
     require('pounce').setup {}
   end,
 })
