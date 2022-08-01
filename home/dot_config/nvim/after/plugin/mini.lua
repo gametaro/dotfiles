a6a563c76c925b0ec3eabbcfe20baee315107b1a
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-08-01 17:55:06 +0900

    chore(mini.cursorword): disable module

--- home/dot_config/nvim/after/plugin/mini.lua
+++ home/dot_config/nvim/after/plugin/mini.lua
@@ -132,23 +132,23 @@ require('mini.comment').setup {
   },
 }
 
-vim.api.nvim_create_autocmd('CursorMoved', {
-  group = group,
-  callback = function(a)
-    local curword = vim.fn.expand('<cword>')
-    local filetype = vim.bo[a.buf].filetype
-
-    local blocklist = {}
-    if filetype == 'lua' then
-      blocklist = { 'local', 'require' }
-    elseif filetype == 'javascript' then
-      blocklist = { 'import' }
-    end
-
-    vim.b.minicursorword_disable = string.len(curword) == 1 or vim.tbl_contains(blocklist, curword)
-  end,
-})
-require('mini.cursorword').setup {}
+-- vim.api.nvim_create_autocmd('CursorMoved', {
+--   group = group,
+--   callback = function(a)
+--     local curword = vim.fn.expand('<cword>')
+--     local filetype = vim.bo[a.buf].filetype
+--
+--     local blocklist = {}
+--     if filetype == 'lua' then
+--       blocklist = { 'local', 'require' }
+--     elseif filetype == 'javascript' then
+--       blocklist = { 'import' }
+--     end
+--
+--     vim.b.minicursorword_disable = string.len(curword) == 1 or vim.tbl_contains(blocklist, curword)
+--   end,
+-- })
+-- require('mini.cursorword').setup {}
 
 require('mini.ai').setup {
   custom_textobjects = {
