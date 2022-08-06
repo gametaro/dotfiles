c80e3f4292d186f4e1b41e7361a670cfb150c5d0
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-08-06 10:29:31 +0900

    refactor(cursorline): tidy up

--- home/dot_config/nvim/plugin/cursorline.lua
+++ home/dot_config/nvim/plugin/cursorline.lua
@@ -1,10 +1,45 @@
+local debounce_leading = require('ky.defer').debounce_leading
+local debounce_trailing = require('ky.defer').debounce_trailing
+
 local group = vim.api.nvim_create_augroup('mine__cursorline', {})
 
+---@class number.Options
+---@field timeout table
+---@field ignore_buftype string[]
+---@field ignore_filetype string[]
+local opts = {
+  timeout = {
+    enable = 300,
+    disable = 300,
+  },
+  ignore_buftype = {
+    'nofile',
+    'prompt',
+  },
+  ignore_filetype = {
+    'TelescopePrompt',
+  },
+}
+
+---@param buf integer
+local ignore = function(buf)
+  return vim.tbl_contains(opts.ignore_buftype, vim.bo[buf].buftype)
+    or vim.tbl_contains(opts.ignore_filetype, vim.bo[buf].filetype)
+end
+
 ---@param value boolean
----@return nil
 local cursorline = function(value)
+  if ignore(vim.api.nvim_get_current_buf()) then return end
   return vim.api.nvim_set_option_value('cursorline', value, { scope = 'local' })
 end
+
+local enable_cursorline = debounce_trailing(function()
+  cursorline(true)
+end, opts.timeout.enable)
+
+local disable_cursorline = debounce_leading(function()
+  cursorline(false)
+end, opts.timeout.disable)
 
 vim.api.nvim_create_autocmd({ 'InsertLeave' }, {
   group = group,
@@ -19,14 +54,6 @@ vim.api.nvim_create_autocmd({ 'InsertEnter', 'WinLeave' }, {
     cursorline(false)
   end,
 })
-
-local disable_cursorline = require('ky.defer').debounce_leading(function()
-  cursorline(false)
-end, 300)
-
-local enable_cursorline = require('ky.defer').debounce_trailing(function()
-  cursorline(true)
-end, 300)
 
 vim.api.nvim_create_autocmd('CursorMoved', {
   group = group,
