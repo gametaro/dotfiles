3bdd3d1491d36bc09813841d9bc5b373ea59da68
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-08-09 14:06:10 +0900

    chore(pounce): remove unneeded code

--- home/dot_config/nvim/after/plugin/pounce.lua
+++ home/dot_config/nvim/after/plugin/pounce.lua
@@ -2,10 +2,3 @@ vim.keymap.set({ 'n', 'x' }, 's', '<Nop>')
 vim.keymap.set({ 'n', 'x' }, 's', '<Cmd>Pounce<CR>')
 vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Cmd>PounceRepeat<CR>')
 vim.keymap.set('o', 'gs', '<Cmd>Pounce<CR>')
-
-vim.api.nvim_create_autocmd('User', {
-  pattern = 'JetpackPounceNvimPost',
-  callback = function()
-    require('pounce').setup {}
-  end,
-})
