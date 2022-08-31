732b1f1140da4fe9a8825689fbc9239f2e033599
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-08-31 11:28:31 +0900

    chore(neogen): add various mappings

--- home/dot_config/nvim/after/plugin/neogen.lua
+++ home/dot_config/nvim/after/plugin/neogen.lua
@@ -3,5 +3,17 @@ if not ok then
   return
 end
 
-require('neogen').setup({ snippet_engine = 'luasnip' })
-vim.keymap.set('n', '<LocalLeader>nf', require('neogen').generate)
+local neogen = require('neogen')
+neogen.setup({ snippet_engine = 'luasnip' })
+vim.keymap.set('n', '<LocalLeader>nf', function()
+  neogen.generate()
+end)
+vim.keymap.set('n', '<LocalLeader>nt', function()
+  neogen.generate({ type = 'type' })
+end)
+vim.keymap.set('n', '<LocalLeader>nc', function()
+  neogen.generate({ type = 'class' })
+end)
+vim.keymap.set('n', '<LocalLeader>nF', function()
+  neogen.generate({ type = 'file' })
+end)
