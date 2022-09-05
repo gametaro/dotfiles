1ef2e9d2903e6d94f48ba9a6b0d9ad7a490bbbfe
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-09-05 09:29:41 +0900

    chore(notify): set `top_down` to false

--- home/dot_config/nvim/after/plugin/notify.lua
+++ home/dot_config/nvim/after/plugin/notify.lua
@@ -5,7 +5,6 @@ end
 
 local notify = require('notify')
 local render = require('notify.render')
-local stages_util = require('notify.stages.util')
 
 notify.setup({
   timeout = 1000,
@@ -16,52 +15,7 @@ notify.setup({
   on_open = function(win)
     vim.api.nvim_win_set_option(win, 'wrap', true)
   end,
-  -- based on built-in `fade`
-  stages = {
-    function(state)
-      local next_height = state.message.height + 2
-      local next_row =
-        stages_util.available_slot(state.open_windows, next_height, stages_util.DIRECTION.BOTTOM_UP)
-      if not next_row then
-        return nil
-      end
-      return {
-        relative = 'editor',
-        anchor = 'NE',
-        width = state.message.width,
-        height = state.message.height,
-        col = vim.opt.columns:get(),
-        row = next_row,
-        border = 'rounded',
-        style = 'minimal',
-        opacity = 0,
-      }
-    end,
-    function()
-      return {
-        opacity = { 100 },
-        col = { vim.o.columns },
-      }
-    end,
-    function()
-      return {
-        col = { vim.o.columns },
-        time = true,
-      }
-    end,
-    function()
-      return {
-        opacity = {
-          0,
-          frequency = 2,
-          complete = function(cur_opacity)
-            return cur_opacity <= 4
-          end,
-        },
-        col = { vim.o.columns },
-      }
-    end,
-  },
+  top_down = false,
 })
 
 vim.notify = notify
