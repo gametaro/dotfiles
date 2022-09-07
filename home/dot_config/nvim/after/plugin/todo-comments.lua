6a597f2295ad4ebdb0890237e32d3e2475483b07
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-09-07 09:08:47 +0900

    feat(todo-comments): add mappings to jump to prev/next comment

--- home/dot_config/nvim/after/plugin/todo-comments.lua
+++ home/dot_config/nvim/after/plugin/todo-comments.lua
@@ -5,5 +5,7 @@ end
 
 vim.keymap.set('n', '<LocalLeader>tq', vim.cmd.TodoQuickFix)
 vim.keymap.set('n', '<LocalLeader>tl', vim.cmd.TodoLocList)
+vim.keymap.set('n', ']t', require('todo-comments').jump_next)
+vim.keymap.set('n', '[t', require('todo-comments').jump_prev)
 
 require('todo-comments').setup()
