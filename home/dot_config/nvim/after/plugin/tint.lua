88cb87d1033b9e06247df357111debc4d991c423
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-09-07 16:56:56 +0900

    chore(tint): add `highlight_ignore_patterns`

--- home/dot_config/nvim/after/plugin/tint.lua
+++ home/dot_config/nvim/after/plugin/tint.lua
@@ -4,6 +4,10 @@ if not ok then
 end
 
 require('tint').setup({
+  highlight_ignore_patterns = {
+    'WinSeparator',
+    'Status.*',
+  },
   window_ignore_function = function(win_id)
     if vim.wo[win_id].diff then
       return true
