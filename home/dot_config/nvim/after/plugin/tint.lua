cda7091801c47eec83e75d5f5d6844e0d113b53d
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-09-07 14:05:46 +0900

    chore(tint): ignore diff window

--- home/dot_config/nvim/after/plugin/tint.lua
+++ home/dot_config/nvim/after/plugin/tint.lua
@@ -3,4 +3,10 @@ if not ok then
   return
 end
 
-require('tint').setup()
+require('tint').setup({
+  window_ignore_function = function(win_id)
+    if vim.wo[win_id].diff then
+      return true
+    end
+  end,
+})
