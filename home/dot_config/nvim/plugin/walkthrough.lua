9037e6456a07262849b5afbec16d0b30acec26aa
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-09-08 16:43:55 +0900

    chore(walkthrough): various improvements

--- home/dot_config/nvim/plugin/walkthrough.lua
+++ home/dot_config/nvim/plugin/walkthrough.lua
@@ -1,16 +1,27 @@
 local api = vim.api
 local fs = vim.fs
 
+local walkthrough = {}
+
+---@alias walkthrough.List.Type 'file'|'directory'
+
+---@class walkthrough.List.Options
+---@field public type walkthrough.List.Type
+---@field public ignore? function
+---@field public sort? function
+
 ---@class walkthrough.Options
 ---@field public next boolean
----@field public type 'file'|'directory'
----@field public silent boolean
+---@field public type walkthrough.List.Type
+---@field public silent? boolean
+---@field public ignore? function
+---@field public sort? function
 
 ---@param t table
----@param v string
+---@param v table
 local index_of = function(t, v)
   for i = 1, #t do
-    if t[i] == v then
+    if vim.inspect(t[i]) == vim.inspect(v) then
       return i
     end
   end
@@ -29,80 +40,140 @@ local prev_index = function(idx, len)
   return idx == 1 and len or idx - 1
 end
 
+---@param a { name: string, type: walkthrough.List.Type }
+---@param b { name: string, type: walkthrough.List.Type }
+---@return boolean
+local sort = function(a, b)
+  if a.type == 'directory' and b.type ~= 'directory' then
+    return true
+  elseif a.type ~= 'directory' and b.type == 'directory' then
+    return false
+  end
+  return a.name < b.name
+end
+
 ---@param path string
----@param type 'file'|'directory'
+---@param opts walkthrough.List.Options
 ---@return table
-local list = function(path, type)
+local list = function(path, opts)
+  opts = opts or {}
   local f = {}
-  for name, _type in fs.dir(path) do
-    if _type == type then
-      f[#f + 1] = name
+  for name, type in fs.dir(path) do
+    if not opts.type or type == opts.type then
+      f[#f + 1] = { name = name, type = type }
     end
   end
+  table.sort(f, opts.sort and opts.sort or sort)
   return f
 end
 
+---@param msg string
+---@param level? integer
+---@param opts? table
+local notify = function(msg, level, opts)
+  opts = opts
+    or {
+      title = 'walkthrough.nvim',
+      on_open = function(win)
+        local buf = api.nvim_win_get_buf(win)
+        api.nvim_buf_set_option(buf, 'filetype', 'markdown_inline')
+      end,
+    }
+  level = level or vim.log.levels.WARN
+  vim.notify(msg, level, opts)
+end
+
 ---@param opts walkthrough.Options
-local walkthrough = function(opts)
+walkthrough.walkthrough = function(opts)
   opts = opts or {}
 
   local full_filename = api.nvim_buf_get_name(0)
-  local filename = fs.basename(full_filename)
+  local basename = fs.basename(full_filename)
   local dirname = fs.dirname(full_filename)
+  local type = vim.fn.isdirectory(full_filename) == 0 and 'file' or 'directory'
   -- would be better if results were cached per directory?
-  local f = list(dirname, opts.type)
+  local f = list(dirname, { type = opts.type, ignore = opts.ignore, sort = opts.sort })
   if #f <= 1 then
+    if not opts.silent then
+      notify('Not found')
+    end
     return
   end
-  local idx = index_of(f, filename)
+  local idx = index_of(f, { name = basename, type = type })
   if not idx then
     if not opts.silent then
-      vim.notify('Not found', vim.log.levels.WARN, { title = 'walkthrough.nvim' })
+      notify(string.format('Not found `%s` in `%s`', basename, vim.inspect(f)))
     end
     return
   end
   local target_idx = opts.next and next_index(idx, #f) or prev_index(idx, #f)
 
-  vim.cmd.edit(dirname .. '/' .. f[target_idx])
+  vim.cmd.edit(dirname .. '/' .. f[target_idx].name)
 end
 
 ---Go to next file in current directory
 ---@param opts walkthrough.Options
-local next_file = function(opts)
+walkthrough.next_file = function(opts)
   opts = opts or {}
   opts.next = true
   opts.type = 'file'
-  walkthrough(opts)
+  walkthrough.walkthrough(opts)
 end
 
 ---Go to previous file in current directory
 ---@param opts walkthrough.Options
-local prev_file = function(opts)
+walkthrough.prev_file = function(opts)
   opts = opts or {}
   opts.next = false
   opts.type = 'file'
-  walkthrough(opts)
+  walkthrough.walkthrough(opts)
 end
 
----Go to previous directory in parent directory
+---Go to previous directory in current directory
 ---@param opts walkthrough.Options
-local next_dir = function(opts)
+walkthrough.next_dir = function(opts)
   opts = opts or {}
   opts.next = true
   opts.type = 'directory'
-  walkthrough(opts)
+  walkthrough.walkthrough(opts)
 end
 
----Go to previous directory in parent directory
+---Go to previous directory in current directory
 ---@param opts walkthrough.Options
-local prev_dir = function(opts)
+walkthrough.prev_dir = function(opts)
   opts = opts or {}
   opts.next = false
   opts.type = 'directory'
-  walkthrough(opts)
+  walkthrough.walkthrough(opts)
 end
 
-vim.keymap.set('n', ']w', next_file, { desc = 'Go to next file in current directory' })
-vim.keymap.set('n', '[w', prev_file, { desc = 'Go to previous file in current directory' })
-vim.keymap.set('n', ']W', next_dir, { desc = 'Go to next directory in parent directory' })
-vim.keymap.set('n', '[W', prev_dir, { desc = 'Go to previous directory in parent directory' })
+---Go to next file/directory in current directory
+---@param opts walkthrough.Options
+walkthrough.next = function(opts)
+  opts = opts or {}
+  opts.next = true
+  walkthrough.walkthrough(opts)
+end
+
+---Go to previous file/directory in current directory
+---@param opts walkthrough.Options
+walkthrough.prev = function(opts)
+  opts = opts or {}
+  opts.next = false
+  walkthrough.walkthrough(opts)
+end
+
+vim.keymap.set(
+  'n',
+  ']w',
+  walkthrough.next,
+  { desc = 'Go to next file/directory in current directory' }
+)
+vim.keymap.set(
+  'n',
+  '[w',
+  walkthrough.prev,
+  { desc = 'Go to previous file/directory in current directory' }
+)
+
+return walkthrough
