958a3b583d21ef19c866dd2b1751e80f020bbe4e
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-09-23 11:47:50 +0900

    refactor(quickhl): use multiple mode mappings

--- home/dot_config/nvim/after/plugin/quickhl.lua
+++ home/dot_config/nvim/after/plugin/quickhl.lua
@@ -1,6 +1,4 @@
-vim.keymap.set('n', '<LocalLeader>m', '<Plug>(quickhl-manual-this-whole-word)')
-vim.keymap.set('x', '<LocalLeader>m', '<Plug>(quickhl-manual-this-whole-word)')
-vim.keymap.set('n', '<LocalLeader>M', '<Plug>(quickhl-manual-reset)')
-vim.keymap.set('x', '<LocalLeader>M', '<Plug>(quickhl-manual-reset)')
+vim.keymap.set({ 'n', 'x' }, '<LocalLeader>m', '<Plug>(quickhl-manual-this-whole-word)')
+vim.keymap.set({ 'n', 'x' }, '<LocalLeader>M', '<Plug>(quickhl-manual-reset)')
 -- vim.keymap.set('n', ']m', '<Plug>(quickhl-manual-go-to-next)')
 -- vim.keymap.set('n', '[m', '<Plug>(quickhl-manual-go-to-prev)')
