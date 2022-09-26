e1915f3584fc1ca28daeb739948606ffdd6b6d5f
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-09-26 16:47:52 +0900

    chore(session-manager): set `autoload_mode` to `CurrentDir`

--- home/dot_config/nvim/after/plugin/session-manager.lua
+++ home/dot_config/nvim/after/plugin/session-manager.lua
@@ -4,5 +4,6 @@ if not ok then
 end
 
 require('session_manager').setup({
+  autoload_mode = require('session_manager.config').AutoloadMode.CurrentDir,
   autosave_ignore_filetypes = { 'gitcommit', 'gitrebase' },
 })
