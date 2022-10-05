5991374b6d504809b423c6ec13b93200f33a29ab
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-10-05 09:27:19 +0900

    chore(hlslens): use <Cmd> for mappings

--- home/dot_config/nvim/after/plugin/hlslens.lua
+++ home/dot_config/nvim/after/plugin/hlslens.lua
@@ -3,16 +3,16 @@ if not ok then
   return
 end
 
-for _, v in ipairs({ 'n', 'N' }) do
-  vim.keymap.set('n', v, function()
-    local ok, msg = pcall(vim.cmd.normal, { vim.v.count1 .. v, bang = true })
-    if ok then
-      require('hlslens').start()
-    else
-      vim.notify(msg, vim.log.levels.INFO, { title = 'nvim-hlslens' })
-    end
-  end)
-end
+vim.keymap.set(
+  'n',
+  'n',
+  [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]]
+)
+vim.keymap.set(
+  'n',
+  'N',
+  [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]]
+)
 vim.keymap.set('', '*', '<Plug>(asterisk-z*)<Cmd>lua require("hlslens").start()<CR>')
 vim.keymap.set('', '#', '<Plug>(asterisk-z#)<Cmd>lua require("hlslens").start()<CR>')
 vim.keymap.set('', 'g*', '<Plug>(asterisk-gz*)<Cmd>lua require("hlslens").start()<CR>')
