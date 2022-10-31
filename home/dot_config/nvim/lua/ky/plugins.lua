552415ea32a97e227656fd9fbca55874bc8c0f57
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-10-31 14:42:23 +0900

    feat(noice): enable lsp.override

--- home/dot_config/nvim/lua/ky/plugins.lua
+++ home/dot_config/nvim/lua/ky/plugins.lua
@@ -194,6 +194,13 @@ packer.startup({
               },
             },
           },
+          lsp = {
+            override = {
+              ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+              ['vim.lsp.util.stylize_markdown'] = true,
+              ['cmp.entry.get_documentation'] = true,
+            },
+          },
           routes = {
             {
               filter = {
