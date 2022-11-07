8aa536e3a7d41c1ef7115a8adafcd9f8b71e41a4
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-11-07 10:21:51 +0900

    chore(unception): hide unception's message

--- home/dot_config/nvim/lua/ky/plugins.lua
+++ home/dot_config/nvim/lua/ky/plugins.lua
@@ -228,6 +228,7 @@ packer.startup({
       'samjwill/nvim-unception',
       setup = function()
         vim.g.unception_open_buffer_in_new_tab = true
+        vim.g.unception_enable_flavor_text = false
       end,
     })
     use({
