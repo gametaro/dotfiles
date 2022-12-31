eeff5a7412370443933377afdee4996a6f1126b2
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2022-12-31 11:44:46 +0900

    chore(ftplugin/markdown): remove unneeded safe require

--- home/dot_config/nvim/after/ftplugin/markdown.lua
+++ home/dot_config/nvim/after/ftplugin/markdown.lua
@@ -3,11 +3,6 @@ vim.opt_local.spell = true
 vim.opt_local.conceallevel = 2
 vim.opt_local.concealcursor = 'nc'
 
-local ok = prequire('nvim-surround')
-if not ok then
-  return
-end
-
 require('nvim-surround').buffer_setup({
   surrounds = {
     ['l'] = {
