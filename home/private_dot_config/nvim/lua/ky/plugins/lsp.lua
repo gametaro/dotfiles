e38a679fa24719bedc97aea4f5c23fdbca198b77
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2023-01-11 16:35:12 +0900

    feat(plugins): add `nvim-ufo`

--- home/private_dot_config/nvim/lua/ky/plugins/lsp.lua
+++ home/private_dot_config/nvim/lua/ky/plugins/lsp.lua
@@ -27,6 +27,10 @@ return {
     })
 
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    capabilities.textDocument.foldingRange = {
+      dynamicRegistration = false,
+      lineFoldingOnly = true,
+    }
 
     ---@param client table
     ---@param bufnr integer
