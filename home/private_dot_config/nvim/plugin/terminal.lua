5c66e21ce2ad10cbc94cf638b4eb8a09519023b9
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2023-01-11 15:53:01 +0900

    fix(statuscolumn): disable on terminal

--- home/private_dot_config/nvim/plugin/terminal.lua
+++ home/private_dot_config/nvim/plugin/terminal.lua
@@ -8,6 +8,7 @@ api.nvim_create_autocmd('TermOpen', {
     vim.opt_local.number = false
     vim.opt_local.relativenumber = false
     vim.opt_local.signcolumn = 'no'
+    vim.opt_local.statuscolumn = ''
 
     ---@param mode string|string[]
     ---@param lhs string
