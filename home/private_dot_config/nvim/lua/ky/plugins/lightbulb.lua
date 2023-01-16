38e86a3f5a67600c7de3695ef1fe4137e7bfcc06
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2023-01-16 11:35:45 +0900

    chore(lightbulb): define autocmd myself

--- home/private_dot_config/nvim/lua/ky/plugins/lightbulb.lua
+++ home/private_dot_config/nvim/lua/ky/plugins/lightbulb.lua
@@ -2,8 +2,11 @@ return {
   'kosayoda/nvim-lightbulb',
   event = 'LspAttach',
   config = function()
-    require('nvim-lightbulb').setup({
-      autocmd = { enabled = true },
+    vim.api.nvim_create_autocmd('CursorHold', {
+      group = vim.api.nvim_create_augroup('mine__lightbulb', {}),
+      callback = function()
+        require('nvim-lightbulb').update_lightbulb()
+      end,
     })
   end,
 }
