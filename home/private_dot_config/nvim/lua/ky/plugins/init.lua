535baa9d278945792bbf533d433c0dc2da33a2d4
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2023-01-18 22:13:08 +0900

    fix(ufo): use treesitter as provider on diff-mode

--- home/private_dot_config/nvim/lua/ky/plugins/init.lua
+++ home/private_dot_config/nvim/lua/ky/plugins/init.lua
@@ -47,7 +47,13 @@ return {
       vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
       vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
 
-      require('ufo').setup()
+      require('ufo').setup({
+        provider_selector = function(bufnr, filetype, buftype)
+          if vim.wo.diff then
+            return { 'treesitter', 'indent' }
+          end
+        end,
+      })
     end,
   },
 }
