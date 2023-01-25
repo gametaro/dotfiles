10890e64e776644a66f5e71c308ce46565273fa9
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2023-01-25 11:43:27 +0900

    refactor(ufo): factor out config

--- home/private_dot_config/nvim/lua/ky/plugins/init.lua
+++ home/private_dot_config/nvim/lua/ky/plugins/init.lua
@@ -36,25 +36,4 @@ return {
   { 'wsdjeg/vim-fetch', event = 'CmdlineEnter' },
   { 'justinmk/vim-gtfo', keys = { 'got', 'gof' } },
   { 'samjwill/nvim-unception', enabled = false, lazy = false },
-  {
-    'kevinhwang91/nvim-ufo',
-    dependencies = 'kevinhwang91/promise-async',
-    event = 'BufReadPre',
-    config = function()
-      vim.o.foldcolumn = '1'
-      vim.o.foldlevel = 99
-      vim.o.foldlevelstart = 99
-      vim.o.foldenable = true
-      vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-      vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
-
-      require('ufo').setup({
-        provider_selector = function(bufnr, filetype, buftype)
-          if vim.wo.diff then
-            return { 'treesitter', 'indent' }
-          end
-        end,
-      })
-    end,
-  },
 }
