c7323f8f38d6b55fb1fd4dd706581e8e07c03f33
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2023-01-25 17:29:54 +0900

    chore(noice): temporarily disable split-related config

--- home/private_dot_config/nvim/lua/ky/plugins/noice.lua
+++ home/private_dot_config/nvim/lua/ky/plugins/noice.lua
@@ -15,14 +15,14 @@ return {
       presets = {
         bottom_search = true,
         command_palette = true,
-        long_message_to_split = true,
+        -- long_message_to_split = true,
         inc_rename = true,
       },
-      views = {
-        split = {
-          enter = true,
-        },
-      },
+      -- views = {
+      --   split = {
+      --     enter = true,
+      --   },
+      -- },
       routes = {
         {
           filter = {
