b7204768cb90ad2fc46747a683640aa6948f7f2d
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2023-02-09 15:36:02 +0900

    refactor(qf): factor out into separate function
    and add annotations

--- home/private_dot_config/nvim/plugin/qf.lua
+++ home/private_dot_config/nvim/plugin/qf.lua
@@ -1,17 +1,25 @@
 local fn = vim.fn
 
----@param info table
+-- |quickfix-window-function|
+---@class Info
+---@field quickfix 1|0
+---@field winid integer
+---@field id integer
+---@field start_idx integer
+---@field end_idx integer
+
+---@param info Info
 ---@return unknown
-local items = function(info)
-  return info.quickfix == 1 and fn.getqflist({ id = info.id, items = 0 }).items
-    or fn.getloclist(info.winid, { id = info.id, items = 0 }).items
+local function items(info)
+  return info.quickfix == 1 and vim.fn.getqflist({ id = info.id, items = 0 }).items
+    or vim.fn.getloclist(info.winid, { id = info.id, items = 0 }).items
 end
 
 ---@param fname string
----@param limit integer
+---@param limit? integer
 ---@return string
-local format_fname = function(fname, limit)
-  fname = fname == '' and '[No Name]' or fn.fnamemodify(fname, ':p:.')
+local function format_fname(fname, limit)
+  fname = fname == '' and '[No Name]' or vim.fn.fnamemodify(fname, ':p:.')
   fname = fname:gsub('^' .. vim.env.HOME, '~')
   limit = limit or 31
 
@@ -22,18 +30,36 @@ local format_fname = function(fname, limit)
   return #fname <= limit and fname_fmt1:format(fname) or fname_fmt2:format(fname:sub(1 - limit))
 end
 
+-- |getqflist()|
+---@class Item
+---@field bufnr integer
+---@field module string
+---@field lnum integer
+---@field end_lnum integer
+---@field col integer
+---@field end_col integer
+---@field vcol boolean
+---@field nr integer
+---@field pattern string
+---@field text string
+---@field type string
+---@field valid boolean
+
+---@param item Item
+local function format(item)
+  local fname = item.bufnr > 0 and format_fname(vim.fn.bufname(item.bufnr)) or ''
+  local lnum = item.lnum > 99999 and -1 or item.lnum
+  local col = item.col > 999 and -1 or item.col
+  local type = item.type == '' and '' or ' ' .. item.type:sub(1, 1):upper()
+  local text = item.text
+  return string.format('%s │%5d:%-3d│%s %s', fname, lnum, col, type, text)
+end
+
 ---@param info table
 ---@return unknown
 function _G.qftf(info)
   return vim.tbl_map(function(item)
-    return item.valid ~= 1 and item.text
-      or (function()
-        local fname = item.bufnr > 0 and format_fname(fn.bufname(item.bufnr)) or ''
-        local lnum = item.lnum > 99999 and -1 or item.lnum
-        local col = item.col > 999 and -1 or item.col
-        local qtype = item.type == '' and '' or ' ' .. item.type:sub(1, 1):upper()
-        return string.format('%s │%5d:%-3d│%s %s', fname, lnum, col, qtype, item.text)
-      end)()
+    return item.valid == 1 and format(item) or item.text
   end, items(info))
 end
 
