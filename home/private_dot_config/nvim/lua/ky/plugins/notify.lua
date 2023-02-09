d94c2bb14df454678240e967fb29238d52e4ce97
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2023-02-09 11:14:19 +0900

    chore(notify): set max_height and max_width

--- home/private_dot_config/nvim/lua/ky/plugins/notify.lua
+++ home/private_dot_config/nvim/lua/ky/plugins/notify.lua
@@ -10,6 +10,12 @@ return {
       end,
       stages = 'fade',
       top_down = false,
+      max_height = function()
+        return math.floor(vim.o.lines * 0.75)
+      end,
+      max_width = function()
+        return math.floor(vim.o.columns * 0.75)
+      end,
     })
   end,
 }
