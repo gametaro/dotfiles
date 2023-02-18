096b79919baba06d3ddc9aa2f1f28aae163b4440
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2023-02-18 18:02:29 +0900

    refactor(number): tidy up

--- home/private_dot_config/nvim/plugin/number.lua
+++ home/private_dot_config/nvim/plugin/number.lua
@@ -1,11 +1,11 @@
-local api = vim.api
-
-local group = api.nvim_create_augroup('mine__number', {})
+local group = vim.api.nvim_create_augroup('mine__number', {})
 
 ---@class number.Options
 ---@field ignore_buftype string[]
 ---@field ignore_filetype string[]
 local opts = {
+  number = true,
+  relativenumber = true,
   ignore_buftype = {
     'help',
     'nofile',
@@ -17,21 +17,23 @@ local opts = {
 }
 
 ---@param value boolean
-local number = function(value)
-  return api.nvim_set_option_value('number', value, { scope = 'local' })
+local function number(value)
+  return vim.api.nvim_set_option_value('number', value, { scope = 'local' })
 end
 
 ---@param value boolean
-local relativenumber = function(value)
-  return api.nvim_set_option_value('relativenumber', value, { scope = 'local' })
+local function relativenumber(value)
+  return vim.api.nvim_set_option_value('relativenumber', value, { scope = 'local' })
 end
 
-local ignore = function(buf)
+---@param buf integer
+---@return boolean
+local function ignore(buf)
   return vim.tbl_contains(opts.ignore_buftype, vim.bo[buf].buftype)
     or vim.tbl_contains(opts.ignore_filetype, vim.bo[buf].filetype)
 end
 
-api.nvim_create_autocmd({ 'InsertEnter', 'WinLeave', 'BufLeave' }, {
+vim.api.nvim_create_autocmd({ 'InsertEnter', 'WinLeave', 'BufLeave' }, {
   group = group,
   callback = function(a)
     if ignore(a.buf) then
@@ -46,14 +48,14 @@ api.nvim_create_autocmd({ 'InsertEnter', 'WinLeave', 'BufLeave' }, {
   end,
 })
 
-api.nvim_create_autocmd({ 'InsertLeave', 'WinEnter', 'BufEnter' }, {
+vim.api.nvim_create_autocmd({ 'InsertLeave', 'WinEnter', 'BufEnter' }, {
   group = group,
   callback = function(a)
     if ignore(a.buf) then
       return
     end
 
-    number(true)
-    relativenumber(true)
+    number(opts.number)
+    relativenumber(opts.relativenumber)
   end,
 })
