99cf2cd5f4c9a6764e3ebef52bdf96b636e9b4ed
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2023-02-18 21:02:07 +0900

    fix(cursorline): fast activate on `WinEnter`

--- home/private_dot_config/nvim/plugin/cursorline.lua
+++ home/private_dot_config/nvim/plugin/cursorline.lua
@@ -1,7 +1,7 @@
 local debounce_leading = require('ky.defer').debounce_leading
 local debounce_trailing = require('ky.defer').debounce_trailing
 
-local group = vim.api.nvim_create_augroup('mine__cursorline', {})
+local group = vim.api.nvim_create_augroup('CursorLine', {})
 
 ---@class cursorline.Options
 ---@field timeout table
@@ -21,18 +21,17 @@ local opts = {
   },
 }
 
----@param buf integer
-local ignore = function(buf)
-  return vim.tbl_contains(opts.ignore_buftype, vim.bo[buf].buftype)
-    or vim.tbl_contains(opts.ignore_filetype, vim.bo[buf].filetype)
-end
-
 ---@param value boolean
-local cursorline = function(value)
-  if ignore(vim.api.nvim_get_current_buf()) then
+local function cursorline(value)
+  local buf = vim.api.nvim_get_current_buf()
+  local win = vim.api.nvim_get_current_win()
+  if
+    vim.tbl_contains(opts.ignore_buftype, vim.bo[buf].buftype)
+    or vim.tbl_contains(opts.ignore_filetype, vim.bo[buf].filetype)
+  then
     return
   end
-  return vim.api.nvim_set_option_value('cursorline', value, { scope = 'local' })
+  return vim.api.nvim_set_option_value('cursorline', value, { scope = 'local', win = win })
 end
 
 local enable_cursorline = debounce_trailing(function()
@@ -43,10 +42,11 @@ local disable_cursorline = debounce_leading(function()
   cursorline(false)
 end, opts.timeout.disable)
 
-vim.api.nvim_create_autocmd({ 'InsertLeave', 'BufEnter' }, {
+vim.api.nvim_create_autocmd({ 'InsertLeave', 'WinEnter' }, {
   group = group,
   callback = function()
     cursorline(true)
+    vim.b.cursorline_disable_defer = true
   end,
 })
 
@@ -60,13 +60,15 @@ vim.api.nvim_create_autocmd({ 'InsertEnter', 'WinLeave' }, {
 vim.api.nvim_create_autocmd('CursorMoved', {
   group = group,
   callback = function()
-    if not vim.b.cursorline then
-      vim.b.cursorline = vim.fn.line('.')
+    local lnum = vim.fn.line('.')
+    if not vim.b.cursorline_lnum then
+      vim.b.cursorline_lnum = lnum
     end
-    if vim.b.cursorline ~= vim.fn.line('.') then
-      vim.b.cursorline = vim.fn.line('.')
+    if vim.b.cursorline_lnum ~= lnum and not vim.b.cursorline_disable_defer then
+      vim.b.cursorline_lnum = lnum
       disable_cursorline()
       enable_cursorline()
     end
+    vim.b.cursorline_disable_defer = false
   end,
 })
