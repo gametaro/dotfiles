7fc517e7f00dfe1bbcaf23ae7aec7419e5505d92
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2023-02-20 13:46:21 +0900

    feat(cursorline): add supports for `cursorcolumn`

--- home/private_dot_config/nvim/plugin/cursorline.lua
+++ home/private_dot_config/nvim/plugin/cursorline.lua
@@ -1,13 +1,17 @@
 local debounce_leading = require('ky.defer').debounce_leading
 local debounce_trailing = require('ky.defer').debounce_trailing
 
-local group = vim.api.nvim_create_augroup('CursorLine', {})
+local group = vim.api.nvim_create_augroup('Cursor', {})
 
 ---@class cursorline.Options
----@field timeout table
+---@field cursorline boolean
+---@field cursorcolumn boolean
+---@field timeout { enable: integer, disable: integer }
 ---@field ignore_buftype string[]
 ---@field ignore_filetype string[]
 local opts = {
+  cursorline = true,
+  cursorcolumn = true,
   timeout = {
     enable = 300,
     disable = 300,
@@ -21,8 +25,7 @@ local opts = {
   },
 }
 
----@param value boolean
-local function cursorline(value)
+local function set(option, value)
   local buf = vim.api.nvim_get_current_buf()
   local win = vim.api.nvim_get_current_win()
   if
@@ -31,7 +34,17 @@ local function cursorline(value)
   then
     return
   end
-  return vim.api.nvim_set_option_value('cursorline', value, { scope = 'local', win = win })
+  vim.api.nvim_set_option_value(option, value, { scope = 'local', win = win })
+end
+
+---@param value boolean
+local function cursorline(value)
+  set('cursorline', value)
+end
+
+---@param value boolean
+local function cursorcolumn(value)
+  set('cursorcolumn', value)
 end
 
 local enable_cursorline = debounce_trailing(function()
@@ -42,33 +55,66 @@ local disable_cursorline = debounce_leading(function()
   cursorline(false)
 end, opts.timeout.disable)
 
+local enable_cursorcolumn = debounce_trailing(function()
+  cursorcolumn(true)
+end, opts.timeout.enable)
+
+local disable_cursorcolumn = debounce_leading(function()
+  cursorcolumn(false)
+end, opts.timeout.disable)
+
+local function on_cursor_moved()
+  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+  vim.b.cursor_pos = vim.b.cursor_pos or {}
+  vim.b.cursor_pos = { vim.b.cursor_pos[1] or row, vim.b.cursor_pos[2] or col }
+
+  local cursor_pos = vim.b.cursor_pos
+  if opts.cursorline and cursor_pos[1] ~= row and not vim.b.cursorline_disable_defer then
+    cursor_pos[1] = row
+    disable_cursorline()
+    enable_cursorline()
+  end
+  if opts.cursorcolumn and cursor_pos[2] ~= col and not vim.b.cursorcolumn_disable_defer then
+    cursor_pos[2] = col
+    disable_cursorcolumn()
+    enable_cursorcolumn()
+  end
+  vim.b.cursor_pos = cursor_pos
+  if opts.cursorline then
+    vim.b.cursorline_disable_defer = false
+  end
+  if opts.cursorcolumn then
+    vim.b.cursorcolumn_disable_defer = false
+  end
+end
+
 vim.api.nvim_create_autocmd({ 'InsertLeave', 'WinEnter' }, {
   group = group,
   callback = function()
-    cursorline(true)
-    vim.b.cursorline_disable_defer = true
+    if opts.cursorline then
+      cursorline(opts.cursorline)
+      vim.b.cursorline_disable_defer = true
+    end
+    if opts.cursorcolumn then
+      cursorcolumn(opts.cursorcolumn)
+      vim.b.cursorcolumn_disable_defer = true
+    end
   end,
 })
 
 vim.api.nvim_create_autocmd({ 'InsertEnter', 'WinLeave' }, {
   group = group,
   callback = function()
-    cursorline(false)
+    if opts.cursorline then
+      cursorline(false)
+    end
+    if opts.cursorcolumn then
+      cursorcolumn(false)
+    end
   end,
 })
 
 vim.api.nvim_create_autocmd('CursorMoved', {
   group = group,
-  callback = function()
-    local lnum = vim.fn.line('.')
-    if not vim.b.cursorline_lnum then
-      vim.b.cursorline_lnum = lnum
-    end
-    if vim.b.cursorline_lnum ~= lnum and not vim.b.cursorline_disable_defer then
-      vim.b.cursorline_lnum = lnum
-      disable_cursorline()
-      enable_cursorline()
-    end
-    vim.b.cursorline_disable_defer = false
-  end,
+  callback = on_cursor_moved,
 })
