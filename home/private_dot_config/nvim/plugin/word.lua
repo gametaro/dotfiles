cb7efd46d2c6e69218aa1b8e672d4bfb8209db0e
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2023-02-26 11:40:58 +0900

    refactor(word): remove aliases

--- home/private_dot_config/nvim/plugin/word.lua
+++ home/private_dot_config/nvim/plugin/word.lua
@@ -22,21 +22,17 @@
 --      SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 -- ]]
 
-local api = vim.api
-local cmd = vim.cmd
-local fn = vim.fn
-
 ---@return string
 local current_char = function()
-  local _, col = unpack(api.nvim_win_get_cursor(0))
-  return api.nvim_get_current_line():sub(col + 1, col + 1)
+  local _, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return vim.api.nvim_get_current_line():sub(col + 1, col + 1)
 end
 
 ---@param motion string
 ---@param mode string
 ---@param times integer
-local move = function(motion, mode, times)
-  local firstpos = api.nvim_win_get_cursor(0)
+local function move(motion, mode, times)
+  local firstpos = vim.api.nvim_win_get_cursor(0)
   local newpos = firstpos
   local lastpos
   local lastiterpos
@@ -46,11 +42,11 @@ local move = function(motion, mode, times)
     while true do
       lastpos = newpos
 
-      cmd.execute(string.format([["normal \<Plug>CamelCaseMotion_%s"]], motion))
+      vim.cmd.execute(string.format([["normal \<Plug>CamelCaseMotion_%s"]], motion))
       if vim.o.selection == 'exclusive' and motion == 'e' and (mode == 'v' or mode == 'o') then
-        cmd.normal({ 'h', bang = true })
+        vim.cmd.normal({ 'h', bang = true })
       end
-      newpos = api.nvim_win_get_cursor(0)
+      newpos = vim.api.nvim_win_get_cursor(0)
 
       if vim.regex([[\k]]):match_str(current_char()) then
         break
@@ -62,57 +58,57 @@ local move = function(motion, mode, times)
   end
 
   if motion == 'w' and mode == 'o' and lastiterpos[1] ~= newpos[1] then
-    api.nvim_win_set_cursor(0, firstpos)
-    cmd.normal({ 'v', bang = true })
-    api.nvim_win_set_cursor(0, lastiterpos)
-    cmd.normal({ '$h', bang = true })
+    vim.api.nvim_win_set_cursor(0, firstpos)
+    vim.cmd.normal({ 'v', bang = true })
+    vim.api.nvim_win_set_cursor(0, lastiterpos)
+    vim.cmd.normal({ '$h', bang = true })
   end
 
   if vim.o.selection == 'exclusive' and motion == 'e' and (mode == 'v' or mode == 'o') then
-    cmd.normal({ 'l', bang = true })
+    vim.cmd.normal({ 'l', bang = true })
   end
   if vim.o.selection == 'exclusive' and motion == 'ge' and mode == 'o' then
-    cmd.normal({ 'ol', bang = true })
+    vim.cmd.normal({ 'ol', bang = true })
   end
 end
 
 ---@param motion string
 ---@param mode string
-local word_move = function(motion, mode)
+local function word_move(motion, mode)
   ---@type integer
   local count = vim.v.count1
 
   local exclusive_adjustment = false
   if mode == 'o' and (motion == 'e' or motion == 'ge') then
-    local _mode = api.nvim_get_mode().mode
+    local _mode = vim.api.nvim_get_mode().mode
     if _mode == 'no' then
-      cmd.normal({ 'v', bang = true })
+      vim.cmd.normal({ 'v', bang = true })
     elseif _mode == 'nov' then
-      cmd.normal({ 'v', bang = true })
+      vim.cmd.normal({ 'v', bang = true })
       exclusive_adjustment = true
     elseif mode == 'noV' or mode == [[no\<C-v>]] then
     end
   end
   if mode == 'v' then
-    cmd.normal({ 'gv', bang = true })
+    vim.cmd.normal({ 'gv', bang = true })
   end
 
   move(motion, mode, count)
 
   if exclusive_adjustment then
-    cmd.execute({ [["normal! \<Esc>"]] })
-    if fn.getpos("'<") == fn.getpos("'>") then
+    vim.cmd.execute({ [["normal! \<Esc>"]] })
+    if vim.fn.getpos("'<") == vim.fn.getpos("'>") then
     else
       local original_whichwrap = vim.o.whichwrap
       vim.o.whichwrap = 'h'
-      cmd.normal({ '`>', bang = true })
-      cmd.normal({ 'h', bang = true })
+      vim.cmd.normal({ '`>', bang = true })
+      vim.cmd.normal({ 'h', bang = true })
 
-      if fn.col('.') == fn.col('$') then
-        cmd.normal({ 'h', bang = true })
+      if vim.fn.col('.') == vim.fn.col('$') then
+        vim.cmd.normal({ 'h', bang = true })
       end
       if motion == 'ge' then
-        cmd.normal({ 'v`<', bang = true })
+        vim.cmd.normal({ 'v`<', bang = true })
       end
       vim.o.whichwrap = original_whichwrap
     end
