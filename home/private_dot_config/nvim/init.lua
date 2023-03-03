53c2b96a3c0d90d3524b73d530b7df240391e439
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2023-03-04 00:44:26 +0900

    fix(profile): start up on top of init.lua

--- home/private_dot_config/nvim/init.lua
+++ home/private_dot_config/nvim/init.lua
@@ -1,1 +1,17 @@
+local profile = vim.env.NVIM_PROFILE
+if profile then
+  local profilepath = vim.fn.stdpath('data') .. '/lazy/profile.nvim'
+  vim.opt.runtimepath:prepend(profilepath)
+  local ok, prof = pcall(require, 'profile')
+  if ok then
+    prof.instrument_autocmds()
+    if profile:lower():match('^start') then
+      prof.start('*')
+    else
+      prof.instrument('*')
+    end
+  end
+  vim.opt.runtimepath:remove(profilepath)
+end
+
 require('ky')
--- home/private_dot_config/nvim/lua/ky/init.lua
+++ home/private_dot_config/nvim/lua/ky/init.lua
@@ -1,19 +1,3 @@
-local profile = vim.env.NVIM_PROFILE
-if profile then
-  local profilepath = vim.fn.stdpath('data') .. '/lazy/profile.nvim'
-  vim.opt.runtimepath:prepend(profilepath)
-  local ok, prof = pcall(require, 'profile')
-  if ok then
-    prof.instrument_autocmds()
-    if profile:lower():match('^start') then
-      prof.start('*')
-    else
-      prof.instrument('*')
-    end
-  end
-  vim.opt.runtimepath:remove(profilepath)
-end
-
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ','
 vim.g.nerd = false
