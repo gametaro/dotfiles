f937ffbf5108ba9e2a582b9c0d831dd08eb58761
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2023-03-04 00:02:24 +0900

    refactor(profile): move config into new file

--- home/private_dot_config/nvim/lua/ky/init.lua
+++ home/private_dot_config/nvim/lua/ky/init.lua
@@ -17,26 +17,6 @@ if profile then
   else
     require('profile').instrument('*')
   end
-
-  local function toggle_profile()
-    local prof = require('profile')
-    if prof.is_recording() then
-      prof.stop()
-      vim.ui.input(
-        { prompt = 'Save profile to: ', completion = 'file', default = 'profile.json' },
-        function(filename)
-          if filename then
-            prof.export(filename)
-            vim.notify(string.format('Wrote %s', filename))
-          end
-        end
-      )
-    else
-      prof.start('*')
-    end
-  end
-
-  vim.keymap.set('', '<f1>', toggle_profile)
 end
 
 vim.cmd.colorscheme('heine')
---
+++ home/private_dot_config/nvim/lua/ky/plugins/profile.lua
@@ -0,0 +1,25 @@
+return {
+  'stevearc/profile.nvim',
+  cond = vim.env.NVIM_PROFILE,
+  init = function()
+    local function toggle_profile()
+      local prof = require('profile')
+      if prof.is_recording() then
+        prof.stop()
+        vim.ui.input(
+          { prompt = 'Save profile to: ', completion = 'file', default = 'profile.json' },
+          function(filename)
+            if filename then
+              prof.export(filename)
+              vim.notify(string.format('Wrote %s', filename))
+            end
+          end
+        )
+      else
+        prof.start('*')
+      end
+    end
+
+    vim.keymap.set('', '<f1>', toggle_profile)
+  end,
+}
