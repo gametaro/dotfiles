c3ce612a2f245fea6e73c54ff60f2c789a674f07
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2023-03-06 11:48:11 +0900

    chore(todo-comments): disable signs and use git grep for search

--- home/private_dot_config/nvim/lua/ky/plugins/todo-comments.lua
+++ home/private_dot_config/nvim/lua/ky/plugins/todo-comments.lua
@@ -12,5 +12,38 @@ return {
       require('todo-comments').jump_prev()
     end, { desc = 'Previous todo comment' })
   end,
-  config = true,
+  config = function()
+    require('todo-comments').setup({
+      keywords = {
+        FIX = { signs = false },
+        TODO = { signs = false },
+        HACK = { signs = false },
+        WARN = { signs = false },
+        PERF = { signs = false },
+        NOTE = { signs = false },
+        TEST = { signs = false },
+      },
+      search = vim.fn.executable('git') == 1 and require('ky.util').is_git_repo() and {
+        command = 'git',
+        args = {
+          '--no-pager',
+          'grep',
+          '-I',
+          '-E',
+          '--no-color',
+          '--line-number',
+          '--column',
+        },
+      } or {
+        command = 'rg',
+        args = {
+          '--color=never',
+          '--no-heading',
+          '--with-filename',
+          '--line-number',
+          '--column',
+        },
+      },
+    })
+  end,
 }
