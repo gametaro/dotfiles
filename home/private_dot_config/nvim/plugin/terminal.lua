8d9be0f933b056d8ce996ebbd2257e7f59e0bd72
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2023-03-10 22:06:30 +0900

    refactor(terminal): factor out into function

--- home/private_dot_config/nvim/plugin/terminal.lua
+++ home/private_dot_config/nvim/plugin/terminal.lua
@@ -1,30 +1,35 @@
+---@param buf integer
+local function on_open(buf)
+  vim.opt_local.number = false
+  vim.opt_local.relativenumber = false
+  vim.opt_local.signcolumn = 'no'
+  vim.opt_local.statuscolumn = ''
+
+  vim.b[buf].keep_mode = false
+
+  local escape = [[<C-\><C-n>]]
+  vim.keymap.set('t', ';', function()
+    local line = vim.api.nvim_get_current_line()
+    return string.match(line, '❯%s+$') and string.format('%s:', escape) or ':'
+  end, { buffer = buf, expr = true, desc = 'Cmdline' })
+  vim.keymap.set('t', [[<C-o>]], function()
+    vim.b[buf].keep_mode = true
+    return string.format('%s<C-o>', escape)
+  end, { buffer = buf, expr = true, desc = 'Goto older position' })
+
+  vim.keymap.set('t', '<Esc>', function()
+    local names = { 'nvim', 'fzf' }
+    return require('ky.util').find_proc_in_tree(vim.b[buf].terminal_job_pid, names) and '<Esc>'
+      or escape
+  end, { buffer = buf, expr = true })
+
+  vim.cmd.startinsert()
+end
+
 vim.api.nvim_create_autocmd('TermOpen', {
   pattern = 'term://*',
   callback = function(a)
-    vim.opt_local.number = false
-    vim.opt_local.relativenumber = false
-    vim.opt_local.signcolumn = 'no'
-    vim.opt_local.statuscolumn = ''
-
-    vim.b[a.buf].keep_mode = false
-
-    local escape = [[<C-\><C-n>]]
-    vim.keymap.set('t', ';', function()
-      local line = vim.api.nvim_get_current_line()
-      return string.match(line, '❯%s+$') and string.format('%s:', escape) or ':'
-    end, { buffer = a.buf, expr = true, desc = 'Cmdline' })
-    vim.keymap.set('t', [[<C-o>]], function()
-      vim.b[a.buf].keep_mode = true
-      return string.format('%s<C-o>', escape)
-    end, { buffer = a.buf, expr = true, desc = 'Goto older position' })
-
-    vim.keymap.set('t', '<Esc>', function()
-      local names = { 'nvim', 'fzf' }
-      return require('ky.util').find_proc_in_tree(vim.b[a.buf].terminal_job_pid, names) and '<Esc>'
-        or escape
-    end, { buffer = a.buf, expr = true })
-
-    vim.cmd.startinsert()
+    on_open(a.buf)
   end,
 })
 
