ba387811a7cf0677d0ab4efd66f0861831a79d30
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2023-03-11 16:35:31 +0900

    chore(unception): disable unception_enable_flavor_text

--- home/private_dot_config/nvim/lua/ky/plugins/init.lua
+++ home/private_dot_config/nvim/lua/ky/plugins/init.lua
@@ -26,6 +26,7 @@ return {
     init = function()
       -- vim.g.unception_open_buffer_in_new_tab = true
       vim.g.unception_delete_replaced_buffer = true
+      vim.g.unception_enable_flavor_text = false
     end,
   },
 }
