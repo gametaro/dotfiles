bfbff2e6326cac93e82d0ed7670cb1f73b21a3c6
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2023-03-11 10:50:19 +0900

    chore(treesitter): change event

--- home/private_dot_config/nvim/lua/ky/plugins/treesitter.lua
+++ home/private_dot_config/nvim/lua/ky/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  event = { 'BufReadPost', 'BufNewFile' },
+  event = { 'FileType' },
   dependencies = {
     { 'nvim-treesitter/nvim-treesitter-textobjects' },
     { 'JoosepAlviste/nvim-ts-context-commentstring' },
