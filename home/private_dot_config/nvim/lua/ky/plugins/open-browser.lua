a72e296a8bb58281a37157110b1305ea6e923b44
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2023-03-14 09:55:18 +0900

    chore(open-browser): remove unnecessary config

--- home/private_dot_config/nvim/lua/ky/plugins/open-browser.lua
+++ home/private_dot_config/nvim/lua/ky/plugins/open-browser.lua
@@ -3,7 +3,4 @@ return {
   keys = {
     { 'gx', '<Plug>(openbrowser-smart-search)', mode = { 'n', 'x' }, desc = { 'Open Browser' } },
   },
-  config = function()
-    require('ky.abbrev').cabbrev('ob', 'OpenBrowserSmartSearch')
-  end,
 }
