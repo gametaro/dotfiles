44cc8faca52c673ea5369284204c87bf4244e324
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2023-03-15 17:20:16 +0900

    feat(plugins): add ChatGPT.nvim

---
+++ home/private_dot_config/nvim/lua/ky/plugins/chatgpt.lua
@@ -0,0 +1,19 @@
+return {
+  'jackMort/ChatGPT.nvim',
+  cmd = { 'ChatGPT' },
+  dependencies = {
+    'MunifTanjim/nui.nvim',
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope.nvim',
+  },
+  config = function()
+    require('chatgpt').setup({
+      openai_params = {
+        max_tokens = 1000,
+      },
+      keymaps = {
+        submit = '<M-Enter>',
+      },
+    })
+  end,
+}
