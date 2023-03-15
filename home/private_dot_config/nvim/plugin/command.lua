9a9c0c876ef97c7f8768a6b9beb9db776924c8d5
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2023-03-15 18:43:35 +0900

    fix(command): validate win

--- home/private_dot_config/nvim/plugin/command.lua
+++ home/private_dot_config/nvim/plugin/command.lua
@@ -1,6 +1,9 @@
 vim.api.nvim_create_user_command('FloatWinsClose', function()
   local wins = vim.api.nvim_list_wins()
   for _, win in ipairs(wins) do
+    if not vim.api.nvim_win_is_valid(win) then
+      return
+    end
     if vim.api.nvim_win_get_config(win).relative ~= '' then
       vim.api.nvim_win_close(win, true)
     end
