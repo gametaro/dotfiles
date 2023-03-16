614e968ab1a74cccd920367c39feecf611e9d536
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2023-03-16 14:59:18 +0900

    chore(heirline): show winbar on quickfix

--- home/private_dot_config/nvim/lua/ky/plugins/heirline.lua
+++ home/private_dot_config/nvim/lua/ky/plugins/heirline.lua
@@ -521,7 +521,7 @@ return {
         disable_winbar_cb = function(args)
           local buf = args.buf
           local buftype =
-            vim.tbl_contains({ 'prompt', 'nofile', 'help', 'quickfix' }, vim.bo[buf].buftype)
+            vim.tbl_contains({ 'prompt', 'nofile', 'help' }, vim.bo[buf].buftype)
           local filetype =
             vim.tbl_contains({ 'gitcommit', 'fugitive', 'Trouble', 'packer' }, vim.bo[buf].filetype)
           return (buftype or filetype) and vim.bo.filetype ~= 'lir'
