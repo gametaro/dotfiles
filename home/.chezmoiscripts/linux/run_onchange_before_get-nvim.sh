980508274dc95646cc110a60e4146ef49bda8a2a
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2023-03-20 20:20:31 +0900

    fix(chezmoi): correctly set exec path

--- home/.chezmoiscripts/linux/run_onchange_before_get-nvim.sh
+++ home/.chezmoiscripts/linux/run_onchange_before_get-nvim.sh
@@ -4,6 +4,6 @@ set -eu
 temp=$(mktemp -d)
 cd "$temp"
 curl -sSfL -O https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
-chmod a+x ./nvim.appimage
+chmod u+x ./nvim.appimage
 ./nvim.appimage --appimage-extract
-ln -sf "$temp/nvim.appimage" ~/.local/bin/nvim
+ln -sf "$temp/squashfs-root/usr/bin/nvim" ~/.local/bin/nvim
