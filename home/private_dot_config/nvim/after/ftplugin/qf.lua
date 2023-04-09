3a770c30ecce9ea584a5c193eeca068294d2f107
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2023-04-09 18:17:29 +0900

    fix(qf): correctly set option value

--- home/private_dot_config/nvim/after/ftplugin/qf.lua
+++ home/private_dot_config/nvim/after/ftplugin/qf.lua
@@ -26,6 +26,10 @@ vim.api.nvim_win_call(prevwin, function()
   prevview = vim.fn.winsaveview()
 end)
 
+for _, name in ipairs(vim.tbl_keys(config)) do
+  prevoptions[name] = vim.wo[prevwin][name]
+end
+
 local group = vim.api.nvim_create_augroup('qf', {})
 vim.api.nvim_create_autocmd({ 'CursorMoved' }, {
   group = group,
@@ -46,7 +50,6 @@ vim.api.nvim_create_autocmd({ 'CursorMoved' }, {
         vim.cmd.filetype('detect')
       end)
       for name, value in pairs(config) do
-        prevoptions[name] = vim.wo[prevwin][name]
         vim.api.nvim_set_option_value(name, value, { win = prevwin, scope = 'local' })
       end
       vim.api.nvim_buf_call(item.bufnr, function()
