9e0513ec6f3ffa014489df60224f808797658085
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2023-04-14 13:29:34 +0900

    refactor(neovide): use global

--- home/private_dot_config/nvim/lua/ky/option.lua
+++ home/private_dot_config/nvim/lua/ky/option.lua
@@ -121,7 +121,7 @@ if vim.fn.executable('zsh') == 1 then
 end
 
 -- neovide
-if vim.fn.exists('g:neovide') > 0 then
+if vim.g.neovide then
   vim.g.neovide_cursor_animation_length = 0
   vim.g.neovide_cursor_trail_length = 0
   vim.g.neovide_floating_blur_amount_x = 2.0
