981da85ac3517610433fe4814a770313c8770b65
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2023-06-08 13:16:05 +0900

    chore(dropbar): hide treesitter/lsp

--- home/dot_config/nvim/lua/ky/plugins/dropbar.lua
+++ home/dot_config/nvim/lua/ky/plugins/dropbar.lua
@@ -9,4 +9,34 @@ return {
       end,
     },
   },
+  config = function()
+    local opts = {
+      bar = {
+        sources = function(_, _)
+          local sources = require('dropbar.sources')
+          return {
+            sources.path,
+            {
+              get_symbols = function(buf, cursor)
+                if vim.bo[buf].ft == 'markdown' then
+                  return sources.markdown.get_symbols(buf, cursor)
+                end
+                -- for _, source in ipairs({
+                --   sources.lsp,
+                --   sources.treesitter,
+                -- }) do
+                --   local symbols = source.get_symbols(buf, cursor)
+                --   if not vim.tbl_isempty(symbols) then
+                --     return symbols
+                --   end
+                -- end
+                return {}
+              end,
+            },
+          }
+        end,
+      },
+    }
+    require('dropbar').setup(opts)
+  end,
 }
