fdbbfbb8832d15dd493216b3b8eabf983ba78000
Kotaro Yamada <32237320+gametaro@users.noreply.github.com>
2023-12-19 23:28:09 +0900

    chore(nvim): try lint on `InsertLeave`

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -294,7 +294,7 @@ local function lazy()
           yaml = { 'actionlint' },
           zsh = { 'zsh' },
         }
-        vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
+        vim.api.nvim_create_autocmd({ 'InsertLeave' }, {
           callback = function() require('lint').try_lint() end,
         })
       end,
